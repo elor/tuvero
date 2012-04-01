159cf8c82b6c661db8e392cb9ee204e7450f2e6d
Erik Lorenz <mail@eriklorenz.net>
2012-04-01 16:46:45 +0200

    rewrote player.js to support a new set of properties

--- player.js
+++ player.js
@@ -1,86 +1,63 @@
-function Player(pid, name) {
-  this.name = name.replace('\\', '') || "noname";
-  this.id = Number(pid);
-
+function Player() {
+// fill the player fields with valid stuff. To fill it with real data, read 
+// fromString() below
+  this.name = Player.strings.name;
   this.female = false;
-  this.inactive = false;
-
-  this.S = false;  // Schiesser?
-  this.L = false;  // Leger?
-  this.B = false;  // A or B?
+  this.year = 1900;
+  this.city = Player.strings.city;
+  this.association = Player.strings.association;
 }
-Player.list = [];
 
+// list of used strings as well as some handy functions
+Player.strings = {
+  name: 'noname',
+  female: 'f',
+  male: '',
+  city: 'nowhere',
+  association: 'none',
+  separator: '\\',
+  forbidden: /[\n\\]/g,
+  replacement: '_',
+  nonum: /(^\D*0|[\.,]\d+|\D)/g,  // I like regex: remove leading 0, all characters and fractions
+  nostr: '',
+  
+  encode: function(string) {
+    return string.replace(Player.strings.forbidden, Player.strings.replacement);
+  },
+
+  numdecode: function(string) {
+    return string.replace(Player.strings.nonum, Player.strings.nostr);
+  }
+};
+
+// 
 Player.prototype.toString = function() {
-  return (this.female ? 'F' : '') + (this.S ? 'S' : '') + (this.L ? 'L' : '')
-      + (this.B ? 'B' : '') + (this.inactive ? 'I' : '') + '\\' + this.name;
+  var year = [this.female ? Player.strings.female : Player.strings.male, this.year].join('');
+  return [Player.strings.encode(this.name),
+      year,
+      Player.strings.encode(this.city),
+      Player.strings.encode(this.association)
+    ].join(Player.strings.separator);
 };
 
-// Player.parse
-//
-// This function turns a string or Players (database or file segment) into the
-// current list of players, thereby destroying the current list
-Player.parse = function (string) {
-  Player.list = [];
+// read 
+Player.prototype.fromString = function(string) {
+  var val = string.split(Player.strings.separator);
 
-  if (!string) {
-    return;
+  this.name = val[0] || Player.strings.name;
+
+  //  
+  if (Player.strings.female && val[1] && val[1][0] == Player.strings.female) {
+    this.female = true;
+  }
+  if (Player.strings.male && val[1] && val[1][0] == Player.strings.male) {
+    this.female = false;
   }
 
-  var lines = string.split('\n');
-  var tmp;
-  var o;  // options
-  var p;  // instance of new player created from one line
-  for (var pid in lines) {
-    tmp = lines[pid].split('\\');
-    o = tmp.shift().toUpperCase();
-    p = new Player(pid, tmp[0]);
+  this.year = Number(Player.strings.numdecode(val[1]));
+  this.city = val[2] || Player.strings.city;
+  this.association = val[3] || Player.strings.association;
 
-    for (var i in o) {
-      switch (o[i]) {
-        case 'F':
-          p.female = true;
-          break;
-        case 'S':
-          p.S = true;
-          break;
-        case 'L':
-          p.L = true;
-          break;
-        case 'B':
-          p.B = true;
-          break;
-        case 'I':
-          p.inactive = true;
-          break;
-      }
-    }
-
-    Player.list[pid] = p;
-  }
-};
-
-Player.load = function () {
-  Player.parse(localStorage.getItem('boules.players'));
-};
-
-Player.compose = function() {
-  var lines = [];
-  var p = Player.list;
-  var length = p.length;
-
-  if (length === 0) {
-    return '';
-  }
-  
-  for (var i=0; i < length; ++i) {
-    lines.push(p[i].toString());
-  }
-  
-  return lines.join('\n');
-};
-
-Player.save = function () {
-  localStorage.setItem('boules.players', Player.compose());
-};
+  return this;
+}
 
