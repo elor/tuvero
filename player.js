2d9c4aedbf13cc7310ae0b285ad9cf5a0a2fca36
Erik Lorenz <mail@eriklorenz.net>
2012-04-01 16:58:00 +0200

    optimized player.js

--- player.js
+++ player.js
@@ -1,20 +1,21 @@
-function Player() {
-// fill the player fields with valid stuff. To fill it with real data, read 
-// fromString() below
-  this.name = Player.strings.name;
-  this.female = false;
-  this.year = 1900;
-  this.city = Player.strings.city;
-  this.association = Player.strings.association;
+function Player(string) {
+  if (string && typeof(string) == "string") {
+    this.fromString(string);
+  }
+}
+
+Player.prototype = {
+  name: 'noname',
+  female: false,
+  year: 1900,
+  city: '',
+  association: ''
 }
 
 // list of used strings as well as some handy functions
-Player.strings = {
-  name: 'noname',
+Player.consts = {
   female: 'f',
   male: '',
-  city: 'nowhere',
-  association: 'none',
   separator: '\\',
   forbidden: /[\n\\]/g,
   replacement: '_',
@@ -22,41 +23,47 @@ Player.strings = {
   nostr: '',
   
   encode: function(string) {
-    return string.replace(Player.strings.forbidden, Player.strings.replacement);
+    return string.replace(Player.consts.forbidden, Player.consts.replacement);
   },
 
   numdecode: function(string) {
-    return string.replace(Player.strings.nonum, Player.strings.nostr);
+    return string.replace(Player.consts.nonum, Player.consts.nostr);
   }
 };
 
 // 
 Player.prototype.toString = function() {
-  var year = [this.female ? Player.strings.female : Player.strings.male, this.year].join('');
-  return [Player.strings.encode(this.name),
+  var year = [this.female ? Player.consts.female : Player.consts.male, this.year].join('');
+  return [Player.consts.encode(this.name),
       year,
-      Player.strings.encode(this.city),
-      Player.strings.encode(this.association)
-    ].join(Player.strings.separator);
+      Player.consts.encode(this.city),
+      Player.consts.encode(this.association)
+    ].join(Player.consts.separator);
 };
 
 // read 
 Player.prototype.fromString = function(string) {
-  var val = string.split(Player.strings.separator);
+  var val = string.split(Player.consts.separator);
 
-  this.name = val[0] || Player.strings.name;
+  if (val[0]) {
+    this.name = val[0];
+  }
 
-  //  
-  if (Player.strings.female && val[1] && val[1][0] == Player.strings.female) {
+  if (Player.consts.female && val[1] && val[1][0] == Player.consts.female) {
     this.female = true;
   }
-  if (Player.strings.male && val[1] && val[1][0] == Player.strings.male) {
-    this.female = false;
+
+  if (val[1]) {
+    this.year = Number(Player.consts.numdecode(val[1]));
   }
 
-  this.year = Number(Player.strings.numdecode(val[1]));
-  this.city = val[2] || Player.strings.city;
-  this.association = val[3] || Player.strings.association;
+  if (val[2]) {
+    this.city = val[2];
+  }
+
+  if (val[3]) {
+    this.association = val[3];
+  }
 
   return this;
 }
