e63602b1a6fb09fe7e157132743d5d4a644c8a6c
Erik Lorenz <mail@eriklorenz.net>
2012-04-06 14:47:18 +0200

    relative ranking data introduced

--- ranking.js
+++ ranking.js
@@ -4,16 +4,52 @@ function Ranking() {
   this.netto = [];
 }
 
+Ranking.ArrayDivision = function(a, b) {
+  var length = b.length;
+  var ret = [];
+  var i;
+
+  if (a.length > b.length) {
+    length = a.length;
+  }
+
+  for (i = 0; i < length; ++i) {
+    if (b[i] && a[i] !== undefined) {
+      ret[i] = a[i] / b[i];
+    }
+  }
+
+  return ret;
+}
+
 Ranking.prototype.process = function() {
   var buchholz = this.games.multvec(this.wins);
-  var feinbuchholz = this.games.multvec(buchholz);
-
-  return {
+  var absolute = {
     games: this.games.linesum(),
     wins: this.wins,
     buchholz: buchholz,
-    feinbuchholz: feinbuchholz,
+    feinbuchholz: this.games.multvec(buchholz),
     netto: this.netto
   };
+
+  var relative = {
+    games: absolute.games,
+    wins: Ranking.ArrayDivision(absolute.wins, absolute.games),
+    buchholz: Ranking.ArrayDivision(absolute.buchholz, absolute.games),
+    feinbuchholz: Ranking.ArrayDivision(absolute.feinbuchholz, absolute.games),
+    netto: Ranking.ArrayDivision(absolute.netto, absolute.games)
+  };
+
+  return {
+    absolute: absolute,
+    relative: relative
+  };
 };
+
+// test
+
+var rk = new Ranking();
+rk.games.fromString("0 2 1\n2 0 1\n1 1 0\n");
+rk.wins = [2, 1, 1];
+this.netto = [1, 2, 3];
 
