19819d0e9fc73827861ef85a5275db57e6c3f4c7
Erik Lorenz <mail@eriklorenz.net>
2012-04-16 18:31:16 +0200

    toasts implemented

--- storage.js
+++ storage.js
@@ -9,16 +9,34 @@ var Storage = (function() {
   };
 
   return {
-    get: function(key) {
-      return window.localStorage.getItem(key);
+    get: function(key, toItem, isNumeric) {
+      var ret = window.localStorage.getItem(key);
+
+      if (isNumeric) {
+        ret = elorpack.unpack(ret);
+      }
+      if (toItem) {
+        toItem.fromString(ret);
+      }
+
+      Toast(["'", key, "' restored"].join(''));
+
+      return ret;
     },
-    set: function(key, item) {
+    set: function(key, item, isNumeric) {
       var str = item.toString();
+      console.log(str);
       if (str === undefined) {
         window.localStorage.removeItem(key);
+        Toast(["'", key, "' removed"].join(''));
       } else {
+        if (isNumeric) {
+          str = elorpack.pack(str);
+        }
         window.localStorage.setItem(key, str);
+        Toast(["'", key, "' saved"].join(''));
       }
+
     },
     clear: function() {
       window.localStorage.clear();
