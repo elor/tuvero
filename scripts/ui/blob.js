b6be1f5ba4c9609ad274e716237f5a9c3c7db998
Erik Lorenz <elor@hrz.tu-chemnitz.de>
2013-04-11 01:04:01 +0200

    tab_history: missing updateBoxes() call added to Blob.fromBlob()

--- scripts/ui/blob.js
+++ scripts/ui/blob.js
@@ -43,6 +43,7 @@ define([ './team', './history', './swiss', './tab_teams', './tab_games',
       // update all tabs
       Tab_Teams.updateBoxes();
       Tab_Games.reset();
+      Tab_History.updateBoxes();
 
       Tab_Ranking.clear();
       Tab_Ranking.update(); // attempt ranking update
