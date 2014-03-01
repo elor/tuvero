2123c355a0b43b6d9acd60ba1ebe544b327e7dc2
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-03-01 17:27:23 +0100

    ui tab test added

---
+++ scripts/ui/test/tab.js
@@ -0,0 +1,17 @@
+/*
+ * Tab Test
+ */
+define([ '../../lib/interface', '../tab', '../tab_games', '../tab_history',
+    '../tab_ranking', '../tab_storage', '../tab_teams' ], function (Interface, Tab, Tab_Games, Tab_History, Tab_Ranking, Tab_Storage, Tab_Teams) {
+  QUnit.test("Tab Interfaces", function () {
+
+    QUnit.equal(Interface.isInterface(Tab), true, "Tab is an interface");
+
+    Interface.verbose(true);
+    QUnit.equal(Interface.match(Tab, Tab_Games, 'frm'), true, "Tab_Games interface match");
+    QUnit.equal(Interface.match(Tab, Tab_History, 'frm'), true, "Tab_History interface match");
+    QUnit.equal(Interface.match(Tab, Tab_Ranking, 'frm'), true, "Tab_Ranking interface match");
+    QUnit.equal(Interface.match(Tab, Tab_Storage, 'frm'), true, "Tab_Storage interface match");
+    QUnit.equal(Interface.match(Tab, Tab_Teams, 'frm'), true, "Tab_Teams interface match");
+  });
+});
