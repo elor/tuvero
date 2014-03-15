dd23a1ea431b06d908be3d8fd75d20bb16fce5ad
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-03-15 12:19:07 +0100

    ranking interfaces cleaned and extended

---
+++ scripts/backend/blobber.js
@@ -0,0 +1,15 @@
+/**
+ * Interface for reading and writing to a blob
+ */
+define(function () {
+  var Blobber = {
+    Interface : {
+      toBlob : function () {
+      },
+      fromBlob : function () {
+      }
+    }
+  };
+
+  return Blobber;
+});
