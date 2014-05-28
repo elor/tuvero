3cf3a5711abf6b10bd2d64d2703ed02d5d05748e
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-05-29 01:21:33 +0200

    toast: use console.error as a fallback

--- scripts/ui/toast.js
+++ scripts/ui/toast.js
@@ -21,71 +21,79 @@ define(function () {
   $(function ($) {
     var $hidden, transition, duration, regex;
     // create toast container
-    $('body').append('<div id="toasts"><div class="hidden">ERROR</div></div>');
+    $('body').append('<div id="toasts"><div class="hidden" style="display:none;">ERROR</div></div>');
 
-    // read fade durations from css
-    $hidden = $('#toasts .hidden');
-    regex = /^all ([0-9.]+)s ease-(in|out) ([0-9.]+)s$/;
+    // abort if the style is not set
+    if ($('#toasts').css('position') !== 'absolute') {
+      console.error('Toast: stylesheet not found');
+      Toast = function (str, seconds) {
+        console.error('Toast: ' + str);
+      };
+    } else {
+      // read fade durations from css
+      $hidden = $('#toasts .hidden');
+      regex = /^all ([0-9.]+)s ease-(in|out) ([0-9.]+)s$/;
 
-    transition = $hidden.css('transition');
-    duration = Number(transition.replace(regex, '$1'));
-    fadeout = duration;
+      transition = $hidden.css('transition');
+      duration = Number(transition.replace(regex, '$1'));
+      fadeout = duration;
 
-    $hidden.addClass('toast');
+      $hidden.addClass('toast');
 
-    transition = $hidden.css('transition');
-    duration = Number(transition.replace(regex, '$1'));
-    fadein = duration;
+      transition = $hidden.css('transition');
+      duration = Number(transition.replace(regex, '$1'));
+      fadein = duration;
 
-    if (fadein != Number(fadein) || !isFinite(fadein) || isNaN(fadein)) {
-      console.error('fadein: not a valid number: ' + fadein);
-      $hidden.removeClass('hidden');
-    }
-
-    if (fadeout != Number(fadeout) || !isFinite(fadeout) || isNaN(fadeout)) {
-      console.error('fadeout: not a valid number: ' + fadeout);
-      $hidden.removeClass('hidden');
-    }
-
-    // actual Toast constructor
-    Toast = function (str, seconds) {
-      var $toasts, $div, $br;
-
-      // default to two seconds duration
-      seconds = seconds || 2;
-
-      $toasts = $('#toasts');
-      $div = $('<div></div>');
-      $br = $('<br>');
-
-      // decide between text and jquery object handle
-      if (typeof str === 'string') {
-        $div.text(str);
-      } else {
-        $div.append(str);
+      if (fadein != Number(fadein) || !isFinite(fadein) || isNaN(fadein)) {
+        console.error('fadein: not a valid number: ' + fadein);
+        $hidden.show();
       }
 
-      // insert the toast and a line break
-      $toasts.append($div);
-      $div.after($br);
+      if (fadeout != Number(fadeout) || !isFinite(fadeout) || isNaN(fadeout)) {
+        console.error('fadeout: not a valid number: ' + fadeout);
+        $hidden.show();
+      }
 
-      // fadein timeout
-      window.setTimeout(function () {
-        $div.addClass('toast');
-      }, 10);
+      // actual Toast constructor
+      Toast = function (str, seconds) {
+        var $toasts, $div, $br;
 
-      // fadeout timeout
-      // TODO on toast fadeout: move whole column upwards
-      window.setTimeout(function () {
-        $div.removeClass('toast');
-      }, 1000 * (seconds + fadein));
+        // default to two seconds duration
+        seconds = seconds || 2;
 
-      // remove timeout
-      window.setTimeout(function () {
-        $br.remove();
-        $div.remove();
-      }, 1000 * (seconds + fadein + fadeout));
-    };
+        $toasts = $('#toasts');
+        $div = $('<div></div>');
+        $br = $('<br>');
+
+        // decide between text and jquery object handle
+        if (typeof str === 'string') {
+          $div.text(str);
+        } else {
+          $div.append(str);
+        }
+
+        // insert the toast and a line break
+        $toasts.append($div);
+        $div.after($br);
+
+        // fadein timeout
+        window.setTimeout(function () {
+          $div.addClass('toast');
+        }, 10);
+
+        // fadeout timeout
+        // TODO on toast fadeout: move whole column upwards
+        window.setTimeout(function () {
+          $div.removeClass('toast');
+        }, 1000 * (seconds + fadein));
+
+        // remove timeout
+        window.setTimeout(function () {
+          $br.remove();
+          $div.remove();
+        }, 1000 * (seconds + fadein + fadeout));
+      };
+    }
 
     // issue any pending toasts
     pending.forEach(function (toast) {
