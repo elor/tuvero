ec6c415c270a3f1c67a4560ea1e3bfb038e98a52
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-05-30 13:14:36 +0200

    players: added string parsing capability for autocomplete fileloading

--- scripts/ui/players.js
+++ scripts/ui/players.js
@@ -9,6 +9,46 @@ define(function () {
 
   Player = {};
 
+  function trimName (name) {
+    return name.replace(/^\s*|\s*$|\n|\r/g, '').replace(/\s\s*/g, ' ');
+  }
+
+  function updateDependencies () {
+    require('./autocomplete').update();
+    // TODO update storage
+  }
+
+  Player.fromString = function (string) {
+    var lines, name;
+
+    lines = string.split('\n');
+
+    // strip the names from white spaces
+    for (name in lines) {
+      lines[name] = trimName(lines[name]);
+    }
+
+    // remove empty entries
+    for (name = lines.length - 1; name >= 0; name -= 1) {
+      if (lines[name].length === 0) {
+        lines.splice(name, 1);
+      }
+    }
+
+    // sort names
+    lines.sort();
+
+    // set names
+    names = lines;
+
+    // update
+    updateDependencies();
+  };
+
+  Player.toString = function () {
+    return names.join('\n');
+  };
+
   Player.fromBlob = function (blob) {
     var blobnames = JSON.parse(blob);
     if (!blobnames) {
@@ -20,7 +60,7 @@ define(function () {
     blobnames.sort();
     names = blobnames;
 
-    require('./autocomplete').update();
+    updateDependencies();
   };
 
   Player.toBlob = function () {
@@ -29,6 +69,8 @@ define(function () {
 
   Player.clear = function () {
     names = [];
+
+    updateDependencies();
   };
 
   Player.get = function () {
@@ -36,10 +78,12 @@ define(function () {
   };
 
   Player.insert = function (name) {
-    if (typeof (name) === 'string') {
+    name = trimName(name);
+    if (typeof (name) === 'string' && name.length > 0) {
       if (names.indexOf(name) === -1) {
         names.push(name);
         names.sort();
+        updateDependencies();
       }
     } else {
       console.error('ui/players.js: name is no string: ' + name);
@@ -47,7 +91,15 @@ define(function () {
   };
 
   Player.erase = function (name) {
-    // TODO do we need it?
+    var index;
+    name = trimName(name);
+    if (typeof (name) === 'string' && name.length > 0) {
+      index = names.indexOf(name);
+      if (index !== -1) {
+        names.splice(index, 1);
+        updateDependencies();
+      }
+    }
   };
 
   return Player;
