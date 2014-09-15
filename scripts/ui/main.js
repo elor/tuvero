821ccbdc703833c12539bfcd0d2e5a7bbf6f5a5c
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-09-15 21:04:57 +0200

    load errors handled

--- scripts/ui/main.js
+++ scripts/ui/main.js
@@ -1,53 +1,4 @@
-define([ './backgroundscripts', './splash', './toast', './strings',
-    './storage', './tab_settings', './tabshandle', './alltabs', './tab_debug',
-    './update' ], function (undefined, Splash, Toast, Strings, Storage, Tab_Settings, Tabshandle, Alltabs, Tab_Debug, Update) {
-
-  // actual initializations are started after any other module has been set
-  // up, hence the jquery function.
-  $(function ($) {
-
-    // look for updates
-    Update();
-    // update Tab_Debug for dev version notifications
-    Tab_Debug.update();
-
-    // TODO show loading screen
-    // TODO show tabs only after everything has been set up
-
-    Alltabs.reset();
-
-    Storage.enable();
-
-    Splash.loading();
-
-    // using a timeout to let the browser update the splashtext
-    setTimeout(function () {
-      try {
-        if (Storage.restore()) {
-          new Toast(Strings.loaded);
-        } else {
-          new Toast(Strings.newtournament);
-        }
-
-        Splash.update();
-
-        Alltabs.update();
-
-        setTimeout(function () {
-          Toast.init();
-          Splash.hide();
-          Tabshandle.valid();
-        }, 10);
-      } catch (e) {
-        console.error('Storage.restore() error caught');
-        console.error(e);
-        Splash.error();
-      }
-    }, 1);
-  });
-});
-
-requirejs.onError = function (err) {
+function notifyAboutLoadError (err) {
   $(function ($) {
     // Splash.setState(), but without splash being loaded
     $('body').addClass('splash');
@@ -56,5 +7,64 @@ requirejs.onError = function (err) {
     $splash.addClass('loaderror');
   });
 
-  throw err;
-};
+  console.log(err);
+}
+
+define([ './backgroundscripts', './splash', './toast', './strings',
+    './storage', './tab_settings', './tabshandle', './alltabs', './tab_debug',
+    './update' ], function (undefined, Splash, Toast, Strings, Storage, Tab_Settings, Tabshandle, Alltabs, Tab_Debug, Update) {
+
+  // actual initializations are started after any other module has been set
+  // up, hence the jquery function.
+  $(function ($) {
+
+    try {
+      // look for updates
+      Update();
+      // update Tab_Debug for dev version notifications
+      Tab_Debug.update();
+
+      // TODO show loading screen
+      // TODO show tabs only after everything has been set up
+
+      Alltabs.reset();
+
+      Storage.enable();
+
+      Splash.loading();
+
+      // using a timeout to let the browser update the splashtext
+      setTimeout(function () {
+        try {
+          if (Storage.restore()) {
+            new Toast(Strings.loaded);
+          } else {
+            new Toast(Strings.newtournament);
+          }
+
+          Splash.update();
+
+          Alltabs.update();
+
+          setTimeout(function () {
+            try {
+              Toast.init();
+              Splash.hide();
+              Tabshandle.valid();
+            } catch (e) {
+              notifyAboutLoadError(e);
+            }
+          }, 10);
+        } catch (e) {
+          console.error('Storage.restore() error caught');
+          console.error(e);
+          Splash.error();
+        }
+      }, 1);
+    } catch (e) {
+      notifyAboutLoadError(e);
+    }
+  });
+});
+
+requirejs.onError = notifyAboutLoadError;
