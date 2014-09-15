5d518a9be2c68b28d92ddb44d5bb48a45adab7c2
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-09-15 20:48:36 +0200

    update.js reworked

--- scripts/ui/update.js
+++ scripts/ui/update.js
@@ -6,34 +6,50 @@ define([ './strings', './toast', './debug' ], function (Strings, Toast, Debug) {
 
   appCache = window.applicationCache;
 
+  function setCached (cached) {
+    if (cached) {
+      Update.isCached = true;
+    } else {
+      Update.isCached = false;
+      if (!Debug.isDevVersion) {
+        console.error('no cache manifest found!');
+        new Toast(Strings.nomanifest, Toast.INFINITE);
+      }
+    }
+  }
+
   function cacheStatus () {
     switch (appCache.status) {
+    case appCache.OBSOLETE:
     case appCache.UNCACHED:
       // no cache manifest available
+      setCached(false);
       break;
+
+    case appCache.UPDATEREADY:
+      appCache.swapCache();
+      setCached(true);
+      new Toast(Strings.updateavailable, Toast.INFINITE);
     case appCache.IDLE:
-      // everything up-to-date
+      setCached(true);
       break;
     case appCache.CHECKING:
-      // downloading and checking manifest
-      break;
     case appCache.DOWNLOADING:
-      // downloading new files
-      // new Toast(Strings.updatedownloading);
-      break;
-    case appCache.UPDATEREADY:
-      // finished downloading. ready to swap the cache
-      appCache.swapCache();
-      new Toast(Strings.updateavailable, Toast.INFINITE);
-      break;
-    case appCache.OBSOLETE:
-      // new version of the manifest was uploaded during download of the files.
       break;
     default:
       console.error('unhandled appCache status: ' + appCache.status);
+      setCached(false);
       break;
     }
   }
+
+  function cacheError () {
+    console.error('unexpected applicationCache error. window.applicationCache.status = ' + window.applicationCache.status);
+  }
+
+  appCache.addEventListener('error', cacheStatus);
+  appCache.addEventListener('cached', cacheStatus);
+  appCache.addEventListener('noupdate', cacheStatus);
   appCache.addEventListener('updateready', cacheStatus);
 
   // function cacheError () {
@@ -42,45 +58,11 @@ define([ './strings', './toast', './debug' ], function (Strings, Toast, Debug) {
   // appCache.addEventListener('error', cacheError);
 
   Update = function () {
-    switch (appCache.status) {
-    case appCache.DOWNLOADING:
-      console.log(' appCache.DOWNLOADING');
-      break;
-    case appCache.CHECKING:
-      console.log(' appCache.CHECKING');
-      break;
-    case appCache.IDLE:
-      console.log(' appCache.IDLE');
-      break;
-    case appCache.OBSOLETE:
-      console.log(' appCache.OBSOLETE');
-      break;
-    case appCache.UNCACHED:
-      console.log(' appCache.UNCACHED');
-      break;
-    case appCache.UPDATEREADY:
-      console.log(' appCache.UPDATEREADY');
-      break;
-    }
-
-    if (appCache.status != appCache.UNCACHED) {
-      if (appCache.status == appCache.IDLE || appCache.status == appCache.UPDATEREADY) {
-        try {
-          appCache.update();
-        } catch (e) {
-          new Toast("AppCache error", Toast.LONG);
-          console.error(e);
-        }
-      }
-      Update.isCached = true;
-    } else {
-      Update.isCached = false;
-
-      if (!Debug.isDevVersion) {
-        console.error('no cache manifest found!');
-
-        new Toast(Strings.nomanifest, Toast.INFINITE);
-      }
+    try {
+      appCache.update();
+    } catch (e) {
+      new Toast("AppCache error", Toast.LONG);
+      console.error(e);
     }
   };
 
