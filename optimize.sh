6c021536d868c93729c960c19663a914e9ad5a6e
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-10-05 17:47:06 +0200

    optimize.sh does not run rmdir images anymore

--- optimize.sh
+++ optimize.sh
@@ -22,7 +22,7 @@ r.js -o scripts/build.js || exit 1
 # delete the now-optimized files and copy them from the build directory #
 #########################################################################
 find images -type f -not -name '*.gif' -not -name 'sprite.png' -not -name 'games.png' -print0 | xargs -0 rm
-find images -type d -print0 | xargs -0 -n1 rmdir
+find images -type d -not -path images -print0 | xargs -0 -n1 rmdir
 rm style scripts -r
 cp -r ../boules-build/{scripts,style} . || exit 1
 
