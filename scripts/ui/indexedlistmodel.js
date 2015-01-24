0fe68fa619e9083b877cec9a2855f407e771fc55
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-01-24 12:20:25 +0100

    IndexedListModel (for keeping IndexedModel indices up to date) added

---
+++ scripts/ui/indexedlistmodel.js
@@ -0,0 +1,66 @@
+/**
+ * A ListModel, which also adjusts the ids using setID
+ * 
+ * @exports IndexedListModel
+ * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
+ * @license MIT License
+ * @see LICENSE
+ */
+
+define([ 'lib/extend', './listmodel' ], function (extend, ListModel) {
+  /**
+   * Constructor for an empty list
+   */
+  function IndexedListModel () {
+    IndexedListModel.superconstructor.call(this);
+  }
+  extend(IndexedListModel, ListModel);
+
+  /**
+   * update the ids, starting at the specified index
+   * 
+   * @param startindex
+   *          the index with which to start. Defaults to 0
+   */
+  IndexedListModel.prototype.updateIDs = function (startindex) {
+    var index;
+
+    startindex = startindex || 0;
+
+    for (index = startindex; index < this.length; index += 1) {
+      this.get(index).setID(index);
+    }
+  };
+
+  /**
+   * Callback function
+   * 
+   * @param emitter
+   *          should be equal to this
+   * @param event
+   *          should be equal to 'insert'
+   * @param data
+   *          a data object containing 'id' and 'object' fields
+   */
+  IndexedListModel.prototype.oninsert = function (emitter, event, data) {
+    ListModel.prototype.oninsert.call(this, emitter, event, data);
+    this.updateIDs(data.id);
+  };
+
+  /**
+   * Callback function
+   * 
+   * @param emitter
+   *          should be equal to this
+   * @param event
+   *          should be equal to 'remove'
+   * @param data
+   *          a data object containing 'id' and 'object' fields
+   */
+  IndexedListModel.prototype.onremove = function (emitter, event, data) {
+    ListModel.prototype.onremove.call(this, emitter, event, data);
+    this.updateIDs(data.id);
+  };
+
+  return IndexedListModel;
+});
---
+++ scripts/ui/test/indexedlistmodel.js
@@ -0,0 +1,48 @@
+/**
+ * Unit tests for IndexedListModel
+ * 
+ * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
+ * @license MIT License
+ * @see LICENSE
+ */
+define(function () {
+  return function (QUnit, getModule) {
+    var extend, IndexedListModel, IndexedModel, ListModel;
+
+    extend = getModule('lib/extend');
+    IndexedListModel = getModule('ui/indexedlistmodel');
+    ListModel = getModule('ui/listmodel');
+    IndexedModel = getModule('ui/indexedmodel');
+
+    QUnit.test("IndexedListModel tests", function () {
+      var list, ret;
+
+      QUnit.ok(extend.isSubclass(IndexedListModel, ListModel), 'IndexedListModel is subclass of ListModel');
+
+      list = new IndexedListModel();
+
+      list.push(new IndexedModel());
+      list.push(new IndexedModel());
+      list.push(new IndexedModel());
+      QUnit.equal(list.get(0).getID(), 0, 'push into empty indexed list sets the proper id');
+      QUnit.equal(list.get(1).getID(), 1, 'push into empty indexed list sets the proper id');
+      QUnit.equal(list.get(2).getID(), 2, 'push into empty indexed list sets the proper id');
+
+      list.pop();
+      QUnit.equal(list.get(0).getID(), 0, 'pop does not affect any indices');
+      QUnit.equal(list.get(1).getID(), 1, 'pop does not affect any indices');
+
+      list.push(new IndexedModel());
+      list.push(new IndexedModel());
+
+      list.remove(0);
+      QUnit.equal(list.get(0).getID(), 0, 'remove(0) adjusts all following indices');
+      QUnit.equal(list.get(1).getID(), 1, 'remove(0) adjusts all following indices');
+      QUnit.equal(list.get(2).getID(), 2, 'remove(0) adjusts all following indices');
+
+      list.remove(1);
+      QUnit.equal(list.get(0).getID(), 0, 'remove(1) adjusts all following indices');
+      QUnit.equal(list.get(1).getID(), 1, 'remove(2) adjusts all following indices');
+    });
+  };
+});
