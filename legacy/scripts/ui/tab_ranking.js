72f3cbfbb50a3e9589b78b60ea542b9c6a6b1ada
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-02-16 19:30:13 +0100

    tab_ranking.js: codestyle

--- legacy/scripts/ui/tab_ranking.js
+++ legacy/scripts/ui/tab_ranking.js
@@ -81,7 +81,7 @@ define(['./tournaments', './team', './toast', './strings', 'options',
     // adjust number of columns to the teamsize
     template.rank.$table.find('th:nth-child(3)').attr('colspan',
         State.teamsize.get());
-    
+
     // hide unimportant columns
     for (i = 0; i < Options.maxteamsize; i += 1) {
       if (i < State.teamsize.get()) {
