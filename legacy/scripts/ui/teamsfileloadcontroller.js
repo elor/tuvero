f1a489eee02c0c4e767166796b5b1393564f249f
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-04-15 12:05:18 +0200

    team file load: utf-8 and latin-1 both work
    By first loading the file as latin-1 and then converting its characters
    into utf-8 where necessary, tuvero can load both utf-8 and latin-1
    files.

--- legacy/scripts/ui/teamsfileloadcontroller.js
+++ legacy/scripts/ui/teamsfileloadcontroller.js
@@ -1,5 +1,13 @@
 /**
- * TeamsFileLoadController
+ * TeamsFileLoadController: loads a whole file and adds its lines as teams
+ *
+ * Inner workings: An HTML5 FileReader reads all contents as ANSI (ISO-8859-1).
+ * It then converts possible UTF-8 byte into utf-8 code points, leaving all
+ * other characters in place. This enables the use of both latin-1 and utf-8 as
+ * input file encoding.
+ *
+ * All other encodings are discouraged. DOS-style line endings are filtered by
+ * "new TeamModel()". MAC-style line endings can still be a problem.
  *
  * TODO rewrite the whole thing and extract a lot of methods
  *
@@ -10,6 +18,112 @@
  */
 define(['lib/extend', 'core/controller', './team', './toast', './strings',
     './state_new'], function(extend, Controller, Team, Toast, Strings, State) {
+
+  function numutfbytes(character) {
+    var code;
+
+    code = character.charCodeAt();
+
+    switch (true) {
+    case code < 0xC0:
+      return 1;
+    case code >= 0xC0 && code < 0xE0:
+      return 2;
+    case code >= 0xE0 && code < 0xF0:
+      return 3;
+    case code >= 0xF0 && code < 0xF8:
+      return 4;
+    case code >= 0xF8 && code < 0xFC:
+      return 5;
+    case code >= 0xFC && code < 0xFE:
+      return 6;
+    }
+  }
+
+  function isutf8byte(character) {
+    var code;
+
+    code = character.charCodeAt();
+
+    return code >= 0x80 && code < 0xC0;
+  }
+
+  function isutf8codepoint(string) {
+    var bytes, byteindex;
+
+    bytes = numutfbytes(string[0]);
+    if (bytes <= 1) {
+      return false;
+    }
+
+    for (byteindex = 1; byteindex < bytes; byteindex += 1) {
+      if (!isutf8byte(string[byteindex])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function latin2utf8symbol(characters) {
+    var bytes, symbol, byteindex;
+
+    bytes = numutfbytes(characters[0]);
+
+    symbol = characters[0].charCodeAt();
+    switch (bytes) {
+    case 1:
+      return characters[0];
+    case 2:
+      symbol = symbol ^ 0xC0;
+      break;
+    case 3:
+      symbol = symbol ^ 0xE0;
+      break;
+    case 4:
+      symbol = symbol ^ 0xF0;
+      break;
+    case 5:
+      symbol = symbol ^ 0xF8;
+      break;
+    case 6:
+      symbol = symbol ^ 0xFC;
+      break;
+    default:
+      return characters[0];
+    }
+
+    console.log(symbol);
+
+    for (byteindex = 1; byteindex < bytes; byteindex += 1) {
+      symbol = symbol << 6;
+      symbol += characters[byteindex].charCodeAt() ^ 0x80;
+    }
+
+    return String.fromCharCode(symbol);
+  }
+
+  function latin2utf8(string) {
+    var symbolindex, ret, symbol;
+    ret = [];
+
+    for (symbolindex = 0; symbolindex < string.length; symbolindex += 1) {
+
+      symbol = string.substr(symbolindex, 6);
+      if (isutf8codepoint(symbol)) {
+        // skip utf8 bytes
+        symbolindex += numutfbytes(symbol) - 1;
+        // add utf-8 codepoint instead of its ansi representation
+        ret.push(latin2utf8symbol(symbol));
+      } else {
+        // just display the ansi symbol
+        ret.push(string[symbolindex]);
+      }
+    }
+
+    return ret.join('');
+  }
+
   /**
    * Constructor
    *
@@ -142,6 +256,10 @@ define(['lib/extend', 'core/controller', './team', './toast', './strings',
 
     contents = evt.target.result;
 
+    contents = latin2utf8(contents);
+
+    filereadercontents = contents;
+
     if (TeamsFileLoadController.createTeamsFromString(contents)) {
       new Toast(Strings.loaded);
     }
@@ -168,7 +286,7 @@ define(['lib/extend', 'core/controller', './team', './toast', './strings',
       reader.teamsfileloadcontroller = controller;
 
       // TODO try different encodings and select the best one
-      reader.readAsText(evt.target.files[0], 'UTF-8');
+      reader.readAsText(evt.target.files[0], 'ISO-8859-1');
     });
   };
 
