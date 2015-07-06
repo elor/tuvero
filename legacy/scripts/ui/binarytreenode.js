7cfffece1840a296fee43eb33f48b3037ba50819
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-07-06 22:12:51 +0200

    BinaryTreeNode: testing and inheritance
    * BinaryTreeNode is a subclass of IndexedModel (for now)
    * BinaryTreeNode tests added

--- legacy/scripts/ui/binarytreenode.js
+++ legacy/scripts/ui/binarytreenode.js
@@ -1,12 +1,14 @@
 /**
- * BinaryTreeNode
+ * BinaryTreeNode: A node of a binary tree, which has a unique ID and supports
+ * querying the ids of parent, left/right children and the sibling.
  *
  * @return BinaryTreeNode
  * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './treenode'], function(extend, TreeNode) {
+define(['lib/extend', 'core/indexedmodel', 'core/type'], function(extend,
+    IndexedModel, Type) {
   /**
    * Constructor
    *
@@ -16,32 +18,13 @@ define(['lib/extend', './treenode'], function(extend, TreeNode) {
   function BinaryTreeNode(id) {
     BinaryTreeNode.superconstructor.call(this);
 
-    this.id = id || 1;
-  }
-  extend(BinaryTreeNode, TreeNode);
-
-  /**
-   * create the two children, if not already present.
-   *
-   * @return true on success, false otherwise
-   */
-  BinaryTreeNode.prototype.createChildren = function() {
-    if (this.children.length === 0) {
-      this.attach(new BinaryTreeNode(this.getLeftChildID));
-      this.attach(new BinaryTreeNode(this.getRightChildID));
-      return true;
-    } else if (this.children.length === 2) {
-      return true;
+    if (Type.isNumber(id)) {
+      this.id = id;
+    } else {
+      this.id = 1;
     }
-    return false;
-  };
-
-  /**
-   * @return the tree-unique ID of this node. Defaults to 1 for the root.
-   */
-  BinaryTreeNode.prototype.getID = function() {
-    return this.id;
-  };
+  }
+  extend(BinaryTreeNode, IndexedModel);
 
   /**
    * @return the ID of this nodes' parent. returns 0 for the root
@@ -54,28 +37,39 @@ define(['lib/extend', './treenode'], function(extend, TreeNode) {
    * @return the ID of this nodes' sibling
    */
   BinaryTreeNode.prototype.getSiblingID = function() {
-    return this.id ^ 0x1;
+    if (this.id > 1) {
+      return this.id ^ 0x1;
+    }
+    return this.id; // root (1) and default (0)
   };
 
   /**
    * @return the ID of this nodes' left child
    */
   BinaryTreeNode.prototype.getLeftChildID = function() {
-    return this.id << 1;
+    if (this.id > 0) {
+      return this.id << 1;
+    } else if (this.id === 0) {
+      return 1;
+    }
+    return 0;
   };
 
   /**
    * @return the ID of this nodes' right child
    */
   BinaryTreeNode.prototype.getRightChildID = function() {
-    return this.id << 1 + 1;
+    return this.getLeftChildID() + (this.id <= 0 ? 0 : 1);
   };
 
   /**
    * @return the depth of this node within the binary tree
    */
   BinaryTreeNode.prototype.getDepth = function() {
-    return Math.floor(Math.log(id) / Math.LN2);
+    if (this.id >= 1) {
+      return Math.floor(Math.log(this.id) / Math.LN2);
+    }
+    return 0;
   };
 
   return BinaryTreeNode;
