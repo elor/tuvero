812c4b769475d7cab81feca1dca8a6668853c7be
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-07-17 15:06:15 +0200

    TeamView supports id-referencing in its constructor

--- legacy/scripts/ui/teamview.js
+++ legacy/scripts/ui/teamview.js
@@ -8,8 +8,7 @@
  * @see LICENSE
  */
 
-define(['lib/extend', 'core/view'], function(extend, View) {
-
+define(['lib/extend', 'core/view', 'core/type'], function(extend, View, Type) {
   /**
    * Constructor
    *
@@ -17,8 +16,14 @@ define(['lib/extend', 'core/view'], function(extend, View) {
    *          a TeamModel instance
    * @param $view
    *          the associated DOM element
+   * @param teamlist
+   *          Optional. A ListModel of TeamModels. If model is a number, model
+   *          is set to teamlist.get(model). Helper for index-referencing lists
    */
-  function TeamView(model, $view) {
+  function TeamView(model, $view, teamlist) {
+    if (Type.isNumber(model) && teamlist !== undefined) {
+      model = teamlist.get(model);
+    }
     TeamView.superconstructor.call(this, model, $view);
 
     this.update();
