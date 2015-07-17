eeae62ca846be9027b61c7ef49fac23637fa8769
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-07-17 10:16:05 +0200

    enable/disable submit button on ties and invalid value

--- legacy/scripts/ui/matchcontroller.js
+++ legacy/scripts/ui/matchcontroller.js
@@ -1,6 +1,8 @@
 /**
  * Controller for finishing a Match
  *
+ * TODO use ValueModel events to communicate the values, not jquery events
+ *
  * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
  * @license MIT License
  * @see LICENSE
@@ -17,13 +19,21 @@ define(['lib/extend', 'core/controller', 'options'], function(extend,
   function MatchController(view) {
     MatchController.superconstructor.call(this, view);
 
+    this.$form = this.view.$view.find('form.finish');
+    this.$submitbutton = this.$form.find('button');
+    this.$points = this.$form.find('.points');
+
     this.initNumberValidation();
-    this.initFinishButton();
+    this.initFormSubmission();
+
+    this.updateSubmitButtonStatus();
   }
   extend(MatchController, Controller);
 
   MatchController.prototype.initNumberValidation = function() {
-    this.view.$view.find('.points').change(function() {
+    var controller = this;
+
+    this.$points.change(function() {
       var $this, value, valid;
 
       valid = true;
@@ -46,11 +56,49 @@ define(['lib/extend', 'core/controller', 'options'], function(extend,
       } else {
         $this.addClass('invalid');
       }
+
+      controller.updateSubmitButtonStatus();
     }).attr('min', Options.minpoints).attr('max', Options.maxpoints);
   };
 
-  MatchController.prototype.initFinishButton = function() {
-    this.view.$view.find('.finish').click(this.finish.bind(this));
+  MatchController.prototype.initFormSubmission = function() {
+    var controller = this;
+
+    this.$form.submit(function(e) {
+
+      debugger;
+
+      e.preventDefault();
+      controller.finish();
+      return false;
+    });
+  };
+
+  MatchController.prototype.updateSubmitButtonStatus = function() {
+    var valid, tie, firstpoints;
+
+    valid = this.$points.filter('.invalid').length === 0;
+
+    if (valid) {
+      tie = true;
+      firstpoints = undefined;
+
+      this.$points.each(function() {
+        var points = Number($(this).val());
+        if (firstpoints === undefined) {
+          firstpoints = points;
+        }
+        if (points !== firstpoints) {
+          tie = false;
+        }
+      });
+
+      if (tie) {
+        valid = false;
+      }
+    }
+
+    this.$submitbutton.prop('disabled', !valid);
   };
 
   MatchController.prototype.finish = function() {
