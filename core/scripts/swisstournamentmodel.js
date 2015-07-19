d3b979868078b3ed884fe33bf81d5f52f79c7506
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-07-19 14:48:23 +0200

    SwissTournamentModel match creation works properly now

--- core/scripts/swisstournamentmodel.js
+++ core/scripts/swisstournamentmodel.js
@@ -6,8 +6,9 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './roundtournamentmodel', 'backend/random'], function(
-    extend, RoundTournamentModel, Random) {
+define(['lib/extend', './roundtournamentmodel', 'backend/random',
+    './matchmodel', './byeresult', 'options'], function(extend,
+    RoundTournamentModel, Random, MatchModel, ByeResult, Options) {
   var rng = new Random();
 
   /**
@@ -60,6 +61,121 @@ define(['lib/extend', './roundtournamentmodel', 'backend/random'], function(
   }
 
   /**
+   * @param outMatches
+   * @param outByes
+   * @param rankGroups
+   * @param gamematrix
+   * @param byes
+   * @param numTeams
+   * @return true on success, false otherwise
+   */
+  function traverseByes(outMatches, outByes, rankGroups, gamematrix, byes,
+      numTeams) {
+    var reverseRankGroups;
+
+    if (numTeams % 2) {
+      reverseRankGroups = rankGroups.slice(0).reverse();
+
+      reverseRankGroups.some(function(group) {
+        return group.some(function(teamid, index) {
+          if (byes.get(teamid)) {
+            return false;
+          }
+
+          group.splice(index, 1);
+          if (traverseAndBacktrack(outMatches, rankGroups, gamematrix)) {
+            outByes.push(teamid);
+            return true;
+          }
+
+          group.splice(index, 0, teamid);
+
+          return false;
+        });
+      });
+    }
+
+    return traverseAndBacktrack(outMatches, rankGroups, gamematrix);
+  }
+
+  /**
+   * counts the number of teams remaining in the rank groups
+   *
+   * @param rankGroups
+   *          a rankGroups object, as returned by getRankGroups
+   * @return the number of teams in the rank group
+   */
+  function getRankGroupsTeamCount(rankGroups) {
+    var sum = 0;
+
+    rankGroups.forEach(function(group) {
+      sum += group.length;
+    });
+
+    return sum;
+  }
+
+  /**
+   * @param outMatches
+   * @param rankGroups
+   * @param gamematrix
+   * @return true on success, false otherwise
+   */
+  function traverseAndBacktrack(outMatches, rankGroups, gamematrix) {
+    var currentGroup, secondGroup, teamA, teamB, teamBindex;
+
+    // console.log(getRankGroupsTeamCount(rankGroups));
+    // console.log(JSON.stringify(rankGroups));
+
+    currentGroup = undefined;
+
+    // get firstGroupIndex by searching for the first non-empty group
+    if (!rankGroups.some(function(group) {
+      currentGroup = group;
+      return group.length > 0;
+    })) {
+      // success! there's no team left
+      return true;
+    }
+
+    teamA = currentGroup.shift();
+
+    secondGroup = undefined;
+    teamBindex = undefined;
+    teamB = undefined;
+
+    // try to find a match in the current group or the one after
+    if (!rankGroups.some(function(group) {
+      return group.some(function(team, index) {
+        if (gamematrix.get(teamA, team) === 0) {
+          secondGroup = group;
+          teamB = team;
+          teamBindex = index;
+          return true;
+        }
+        return false;
+      });
+    })) {
+      currentGroup.unshift(teamA);
+      return false;
+    }
+
+    secondGroup.splice(teamBindex, 1);
+
+    if (traverseAndBacktrack(outMatches, rankGroups, gamematrix)) {
+      // don't use push, because the best-ranked team should be listed in the
+      // first match
+      outMatches.unshift([teamA, teamB]);
+      return true;
+    }
+
+    currentGroup.splice(teamBindex, 0, teamB);
+    currentGroup.unshift(teamA);
+
+    return false;
+  }
+
+  /**
    * Constructor
    *
    * @param rankingorder
@@ -74,7 +190,7 @@ define(['lib/extend', './roundtournamentmodel', 'backend/random'], function(
 
   SwissTournamentModel.prototype.SYSTEM = 'swiss';
 
-  SwissTournamentModel.prototype.RANKINGDEPENDENCIES = ['gamematrix'];
+  SwissTournamentModel.prototype.RANKINGDEPENDENCIES = ['byes', 'gamematrix'];
 
   SwissTournamentModel.prototype.ORDERS = ['order', 'halves', 'wingroups',
       'random'];
@@ -85,7 +201,7 @@ define(['lib/extend', './roundtournamentmodel', 'backend/random'], function(
    * @return true on success, false otherwise
    */
   SwissTournamentModel.prototype.idleMatches = function() {
-    var rankGroups;
+    var rankGroups, matches, byes;
 
     if (this.getProperty('initialorder') === 'wingroups') {
       this.emit('error', 'cannot use wingroups at the moments');
@@ -95,7 +211,29 @@ define(['lib/extend', './roundtournamentmodel', 'backend/random'], function(
     rankGroups = getRankGroups(this.ranking.get());
     rankGroups = randomizeRankGroups(rankGroups);
 
-    debugger;
+    matches = [];
+    byes = [];
+    if (!traverseByes(matches, byes, rankGroups, this.ranking.gamematrix,
+        this.ranking.byes, this.teams.length)) {
+      this.emit('Teams have already met or all teams already have a bye');
+      return false;
+    }
+
+    this.round += 1;
+
+    byes.forEach(function(byeTeamID, byeIndex) {
+      // TODO extract method
+      this.votes.bye.push(byeTeamID);
+      this.history.push(new ByeResult(byeTeamID, [Options.byepointswon,
+          Options.byepointslost], matches.length + byeIndex, this.round));
+      this.ranking.bye(byeTeamID);
+    }, this);
+
+    matches.forEach(function(matchTeams, matchid) {
+      this.matches.push(new MatchModel(matchTeams, matchid, this.round));
+    }, this);
+
+    return true;
   };
 
   /**
