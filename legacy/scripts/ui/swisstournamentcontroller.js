88b8c395da64b2da026cf214300b2d46980a283d
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-07-19 16:58:24 +0200

    SwissTournament: mode, shuffle and transpose UI elements

--- legacy/scripts/ui/swisstournamentcontroller.js
+++ legacy/scripts/ui/swisstournamentcontroller.js
@@ -6,14 +6,37 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', 'core/controller'], function(extend, Controller) {
+define(['lib/extend', 'core/controller', 'core/listener'], function(extend,
+    Controller, Listener) {
   /**
    * Constructor
    *
    * @param view
+   *          a SwissTournamentView instance
    */
   function SwissTournamentController(view) {
+    var $mode, $noshuffle, tournament, noshuffle;
+
     SwissTournamentController.superconstructor.call(this, view);
+
+    tournament = this.model.tournament;
+    noshuffle = this.model.noshuffle;
+
+    this.$options = this.view.$view.find('.tournamentoptions');
+    $mode = this.$options.find('select.mode');
+    $noshuffle = this.$options.find('input.noshuffle');
+
+    $mode.change(function() {
+      tournament.setProperty('swissmode', $(this).val());
+      $mode.val($(this).val());
+      tournament.setProperty('swisstranspose', tournament
+          .getProperty('swissmode') === 'halves');
+    });
+
+    Listener.bind(noshuffle, 'update', function() {
+      $noshuffle.prop('checked', noshuffle.get());
+      tournament.setProperty('swissshuffle', !noshuffle.get());
+    });
   }
   extend(SwissTournamentController, Controller);
 
--- legacy/scripts/ui/swisstournamentview.js
+++ legacy/scripts/ui/swisstournamentview.js
@@ -6,8 +6,9 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './tournamentview', './swisstournamentcontroller'],//
-function(extend, TournamentView, SwissTournamentController) {
+define(['lib/extend', './tournamentview', './swisstournamentcontroller',
+    'core/valuemodel', './checkboxview'], function(extend, TournamentView,
+    SwissTournamentController, ValueModel, CheckBoxView) {
   /**
    * Constructor
    *
@@ -18,6 +19,21 @@ function(extend, TournamentView, SwissTournamentController) {
    */
   function SwissTournamentView(model, $view) {
     SwissTournamentView.superconstructor.call(this, model, $view);
+
+    // set noshuffle
+    this.model.noshuffle = new ValueModel(!this.model.tournament
+        .getProperty('swissshuffle'));
+    // use noshuffle checkboxes
+    this.noshufflecheckboxview = {
+      initial: new CheckBoxView(this.model.noshuffle, this.$view
+          .find('.initial .tournamentoptions .option input.noshuffle')),
+      idle: new CheckBoxView(this.model.noshuffle, this.$view
+          .find('.idle .tournamentoptions .option input.noshuffle')),
+    };
+
+    // read the swiss mode
+    this.$view.find('.tournamentoptions .option select.mode').val(
+        this.model.tournament.getProperty('swissmode'));
 
     this.subcontroller = new SwissTournamentController(this);
   }
