27490f9ec339a71d5c84aca5254ef4e194efe6b5
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-08-02 14:10:19 +0200

    CheckBoxView: testing the click next to the checkbox

--- selenium-tests/src/CheckBoxView.java
+++ selenium-tests/src/CheckBoxView.java
@@ -1,17 +1,11 @@
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.google.common.base.Function;
-import com.thoughtworks.selenium.Selenium;
 
 class CheckBoxView implements TuveroTest {
 
@@ -20,6 +14,7 @@ class CheckBoxView implements TuveroTest {
   private WebElement falseButton;
   private WebElement trueButton;
   private WebDriver driver;
+  private WebElement checkboxspan;
 
   @Override
   public void run(TuveroTestRunner runner, String prefix) {
@@ -27,6 +22,7 @@ class CheckBoxView implements TuveroTest {
 
     value = driver.findElement(By.className("checkboxvalue"));
     checkbox = driver.findElement(By.tagName("input"));
+    checkboxspan = driver.findElement(By.className("checkboxspan"));
     List<WebElement> buttons = driver.findElements(By.tagName("button"));
     falseButton = buttons.get(0);
     trueButton = buttons.get(1);
@@ -35,10 +31,13 @@ class CheckBoxView implements TuveroTest {
     runner.equal(trueButton.getText(), "true", "'true' button queried");
 
     /*
-     * wait for the current value to show
+     * wait for the initial value to show
      */
     runner.ok(waitForValue(true), "Pageload");
 
+    /*
+     * click the checkbox and its surrounding span
+     */
     checkbox.click();
     runner.ok(waitForValue(false), "checkbox click changes the value");
     runner.equal(checked(), false, "checkbox is unchecked");
@@ -47,6 +46,17 @@ class CheckBoxView implements TuveroTest {
     runner.ok(waitForValue(true), "checkbox click changes the value again");
     runner.equal(checked(), true, "checkbox is re-checked");
 
+    checkboxspan.click();
+    runner.ok(waitForValue(false), "checkbox-span click changes the value");
+    runner.equal(checked(), false, "checkbox is unchecked");
+
+    checkboxspan.click();
+    runner.ok(waitForValue(true), "checkbox-span click reverts the change");
+    runner.equal(checked(), true, "checkbox is re-checked");
+
+    /*
+     * true/false button clicks (i.e. setting the background state directly)
+     */
     falseButton.click();
     runner.ok(waitForValue(false), "false-button click changes the value");
     runner.equal(checked(), false, "checkbox is unchecked");
