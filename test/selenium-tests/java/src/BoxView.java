976465414959e162e0b36f83976be1bcf45b7405
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-08-02 10:30:29 +0200

    BoxView.java: testing box autogrowth, cleanup refactoring

--- test/selenium-tests/java/src/BoxView.java
+++ test/selenium-tests/java/src/BoxView.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.w3c.css.sac.DescendantSelector;
 
 import com.google.common.base.Function;
 
 class BoxView implements TuveroTest {
+
+  final static private int collapsedHeight = 50;
 
   @Override
   public void run(TuveroTestRunner runner, String prefix) {
@@ -49,46 +50,21 @@ class BoxView implements TuveroTest {
     runner.ok(generatedBox.getAttribute("class").contains("generated"),
         "generated box if marked as being generated");
 
-    final int collapsedHeight = 50;
-
-    runner.ok(firstBox.getSize().height > collapsedHeight,
-        "first box isn't collapsed");
-    runner.ok(secondBox.getSize().height < collapsedHeight,
-        "second box is collapsed");
-    runner.ok(generatedBox.getSize().height > collapsedHeight,
-        "generated box isn't collapsed");
+    runner.notOk(isCollapsed(firstBox), "first box isn't collapsed");
+    runner.ok(isCollapsed(secondBox), "second box is collapsed");
+    runner.notOk(isCollapsed(generatedBox), "generated box isn't collapsed");
 
     /*
-     * click first box
+     * collapse first box
      */
-    firstBox.findElement(By.tagName("h3")).click();
-    try {
-      wait.until(new Function<WebDriver, Boolean>() {
-        @Override
-        public Boolean apply(WebDriver driver) {
-          return firstBox.getSize().height < collapsedHeight;
-        }
-      });
-    } catch (TimeoutException t) {
-    }
-    runner.ok(firstBox.getSize().height < collapsedHeight,
-        "first box collapses on click");
+    toggleCollapse(driver, firstBox);
+    runner.ok(isCollapsed(firstBox), "first box collapses on click");
 
     /*
-     * click second box
+     * expand second box
      */
-    secondBox.findElement(By.tagName("h3")).click();
-    try {
-      wait.until(new Function<WebDriver, Boolean>() {
-        @Override
-        public Boolean apply(WebDriver driver) {
-          return secondBox.getSize().height > collapsedHeight;
-        }
-      });
-    } catch (TimeoutException t) {
-    }
-    runner.ok(secondBox.getSize().height > collapsedHeight,
-        "second box decollapses on click");
+    toggleCollapse(driver, secondBox);
+    runner.notOk(isCollapsed(secondBox), "second box expands on click");
 
     /*
      * try to tab to a hidden input element
@@ -108,23 +84,91 @@ class BoxView implements TuveroTest {
         "tab-focus enter first non-collapsed element");
 
     /*
-     * click second box
+     * focus-loss on box collapse
      */
-    secondBox.findElement(By.tagName("h3")).click();
-    try {
-      wait.until(new Function<WebDriver, Boolean>() {
-        @Override
-        public Boolean apply(WebDriver driver) {
-          return secondBox.getSize().height < collapsedHeight;
-        }
-      });
-    } catch (TimeoutException t) {
-    }
-    runner.ok(secondBox.getSize().height < collapsedHeight,
-        "second box decollapses on click");
+    toggleCollapse(driver, secondBox);
+    runner.ok(isCollapsed(secondBox), "second box collapses while focused");
     focused = driver.switchTo().activeElement();
     runner.equal(focused, body,
         "focus of contained input elements is lost on collapsing");
 
+    /*
+     * boxes auto-grow with their content
+     */
+    int generatedBoxHeight = generatedBox.getSize().height;
+    try {
+      wait.until(new AutoGrowthCondition(generatedBox));
+    } catch (TimeoutException t) {
+    }
+    runner.ok(generatedBox.getSize().height > generatedBoxHeight,
+        "boxes grow with their content");
+
+    /*
+     * collapse generated box
+     */
+    toggleCollapse(driver, generatedBox);
+    runner.ok(isCollapsed(generatedBox), "generated box collapses on click");
+
+    /*
+     * expand generated box again
+     */
+    toggleCollapse(driver, generatedBox);
+    runner.notOk(isCollapsed(generatedBox), "generated box expands on click");
+
+    /*
+     * see if the generated box still grows
+     */
+    generatedBoxHeight = generatedBox.getSize().height;
+    try {
+      wait.until(new AutoGrowthCondition(generatedBox));
+    } catch (TimeoutException t) {
+    }
+    runner.ok(generatedBox.getSize().height > generatedBoxHeight,
+        "boxes grow with their content");
+  }
+
+  private boolean isCollapsed(final WebElement box) {
+    return box.getSize().height < collapsedHeight;
+  }
+
+  private void toggleCollapse(WebDriver driver, final WebElement box) {
+    boolean isCurrentlyCollapsed = isCollapsed(box);
+    box.findElement(By.tagName("h3")).click();
+    WebDriverWait wait = new WebDriverWait(driver, 1);
+    try {
+      wait.until(new CollapsedCondition(box, !isCurrentlyCollapsed));
+    } catch (TimeoutException t) {
+    }
+  }
+
+  private final class AutoGrowthCondition implements
+      Function<WebDriver, Boolean> {
+    private final int generatedBoxHeight;
+    private final WebElement generatedBox;
+
+    private AutoGrowthCondition(WebElement generatedBox) {
+      this.generatedBoxHeight = generatedBox.getSize().height;
+      this.generatedBox = generatedBox;
+    }
+
+    @Override
+    public Boolean apply(WebDriver arg0) {
+      return generatedBox.getSize().height > generatedBoxHeight;
+    }
+  }
+
+  class CollapsedCondition implements Function<WebDriver, Boolean> {
+    private WebElement box;
+    private boolean shouldBeCollapsed;
+
+    public CollapsedCondition(WebElement boxView, boolean shouldBeCollapsed) {
+      this.box = boxView;
+      this.shouldBeCollapsed = shouldBeCollapsed;
+    }
+
+    @Override
+    public Boolean apply(WebDriver driver) {
+      return isCollapsed(box) == shouldBeCollapsed;
+    }
   }
 }
