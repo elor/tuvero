6bd5d3fd9e6886a17c6d962205977abbed9390dd
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-08-14 13:05:41 +0200

    debug-tab: clear all-button works

--- legacy/scripts/ui/debugtab.js
+++ legacy/scripts/ui/debugtab.js
@@ -5,62 +5,69 @@
  * @see LICENSE
  */
 define([ 'lib/extend', 'jquery', 'core/view', 'core/valuemodel', './valueview',
-		'./browser' //
-], function(extend, $, View, ValueModel, ValueView, Browser) {
-	/**
-	 * represents a whole team tab
-	 * 
-	 * TODO write a TabView superclass with common functions
-	 * 
-	 * TODO isolate common tab-related function
-	 * 
-	 * @param $tab
-	 *            the tab DOM element
-	 */
-	function DebugTab($tab) {
-		DebugTab.superconstructor.call(this, undefined, $tab);
+    './browser', './storage', './strings', './toast' ], function(extend, $,
+    View, ValueModel, ValueView, Browser, Storage, Strings, Toast) {
+  /**
+   * represents a whole team tab
+   * 
+   * TODO write a TabView superclass with common functions
+   * 
+   * TODO isolate common tab-related function
+   * 
+   * @param $tab
+   *          the tab DOM element
+   */
+  function DebugTab($tab) {
+    DebugTab.superconstructor.call(this, undefined, $tab);
 
-		this.init();
-	}
-	extend(DebugTab, View);
+    this.init();
+  }
+  extend(DebugTab, View);
 
-	/**
-	 * initialize the tab functionality
-	 * 
-	 * TODO maybe split it into multiple autodetected functions?
-	 */
-	DebugTab.prototype.init = function() {
-		var $container, value;
+  /**
+   * initialize the tab functionality
+   * 
+   * TODO maybe split it into multiple autodetected functions?
+   */
+  DebugTab.prototype.init = function() {
+    var $container, value;
 
-		/*
-		 * show browser name
-		 */
-		value = new ValueModel(Browser.name);
-		$container = this.$view.find('.browser .name');
-		this.browserNameView = new ValueView(value, $container);
+    /*
+     * show browser name
+     */
+    value = new ValueModel(Browser.name);
+    $container = this.$view.find('.browser .name');
+    this.browserNameView = new ValueView(value, $container);
 
-		/*
-		 * show browser version
-		 */
-		value = new ValueModel(Browser.version);
-		$container = this.$view.find('.browser .version');
-		this.browserVersionView = new ValueView(value, $container);
+    /*
+     * show browser version
+     */
+    value = new ValueModel(Browser.version);
+    $container = this.$view.find('.browser .version');
+    this.browserVersionView = new ValueView(value, $container);
 
-		// TODO clear all
+    /*
+     * button: clear all
+     */
+    $container = this.$view.find('.register .delete');
+    $container.click(function() {
+      Storage.clear();
+      Storage.store();
+      new Toast(Strings.reset);
+    });
 
-		// TODO register teams
+    // TODO register teams
+  };
 
-	};
+  // FIXME CHEAP HACK AHEAD
+  $(function($) {
+    var $tab;
 
-	// FIXME CHEAP HACK AHEAD
-	$(function($) {
-		var $tab;
+    $tab = $('#tabs > [data-tab="debug"]');
+    if ($tab.length && $('#testmain').length === 0) {
+      return new DebugTab($tab);
+    }
+  });
 
-		$tab = $('#tabs > [data-tab="debug"]');
-		if ($tab.length && $('#testmain').length === 0) {
-			return new DebugTab($tab);
-		}
-	});
-
-	return DebugTab;
+  return DebugTab;
 });
