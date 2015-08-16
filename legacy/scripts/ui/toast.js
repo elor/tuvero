32ccdc7eb784f20a8eeabeef6922ab383ddb3425
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-08-16 16:03:54 +0200

    Toast: close() works now thanks to a complete rewrite

--- legacy/scripts/ui/toast.js
+++ legacy/scripts/ui/toast.js
@@ -1,194 +1,191 @@
 /**
  * A Toast is a small text which is visible for a short period of time before
  * disappearing.
- *
- * You can supply a jquery object handle as str if you want to
- * insert an html node instead of text.
- *
+ * 
+ * You can supply a jquery object handle as str if you want to insert an html
+ * node instead of text.
+ * 
  * Toast duration can be set at Toast construction or controlled via a close()
  * function
- *
+ * 
  * @return Toast
  * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
  * @license MIT License
  * @see LICENSE
  */
-define(function() {
-  var Toast, fadein, fadeout, pending, toastfn, nextid, fadeoutfn;
+define([ 'jquery', 'core/type' ], function($, Type) {
+  var initialized, pending;
 
   // pending toasts which have been issued before jquery was available
+  initialized = false;
   pending = [];
-  nextid = 0;
 
-  function getid() {
-    var id;
-    id = nextid;
-    nextid += 1;
-    return id;
-  }
+  /**
+   * read the transition durations
+   */
+  function getTransitionDuration() {
+    var transition;
 
-  // will become a function once jquery has been initialized
-  fadeoutfn = undefined;
+    transition = Toast.$template.css('transition');
 
-  toastfn = function(str, seconds) {
-    var id;
-
-    id = getid();
-
-    pending.push({
-      str: str,
-      seconds: seconds,
-      id: id
-    });
-
-    return id;
-  };
-
-  function createCloseFunction(id) {
-    return function() {
-      var i, $alltoasts, $div;
-      if (pending[id]) {
-        pending[id] = undefined;
-      }
-
-      // see if jquery has been initialized
-      if (fadeoutfn) {
-        $alltoasts = $('#toasts .toast');
-        for (i = 0; i < $alltoasts.length; i += 1) {
-          $div = $alltoasts.eq(i);
-          if ($div.data().id === id) {
-            fadeoutfn($div);
-          }
-        }
-      }
-    };
-  }
-
-  // temporary toast constructor
-  Toast = function(str, seconds) {
-    var id;
-
-    id = toastfn(str, seconds);
-
-    this.close = createCloseFunction(id);
-  };
-
-  Toast.init = function() {
-    var $hidden, transition, duration, id;
-    // create toast container
-    $('body').append('<div id="toasts"><div class="hidden" style="display:none;">ERROR</div></div>');
-
-    function getTransitionDuration() {
-      var prefix, prefixes, transition;
-
-      // TODO remove prefixes from this file
-      prefixes = ['', '-o-', '-ms-', '-moz-', '-webkit-'];
-      transition = undefined;
-
-      for (prefix in prefixes) {
-        prefix = prefixes[prefix];
-        transition = $hidden.css(prefix + 'transition');
-        if (transition) {
-          break;
-        }
-      }
-      if (transition === undefined) {
-        console.error("could not read any transition lengths. What's your browser?");
-        return 0.2;
-      }
-
-      // return duration
-      return Number(transition.replace(/^[^0-9]*([0-9.]+)s.*$/, '$1'));
+    if (transition === undefined) {
+      console.error("could not read any transition lengths. "
+          + "What's your browser?");
+      return 0.2;
     }
+
+    // return duration
+    return Number(transition.replace(/^[^0-9]*([0-9.]+)s.*$/, '$1'));
+  }
+
+  /**
+   * toast constructor
+   * 
+   * @param str
+   *          the string of the toast
+   * @param seconds
+   *          Toast duration in seconds. See Toast.SHORT, Toast.LONG and
+   *          Toast.INFINITE. Defaults to Toast.SHORT.
+   */
+  function Toast(message, seconds) {
+    this.message = message;
+    this.duration = seconds || Toast.SHORT;
+
+    this.$toast = undefined;
+
+    if (initialized) {
+      this.display();
+    } else {
+      pending.push(this);
+    }
+  }
+
+  Toast.fadeinDuration = 0.2;
+  Toast.fadeoutDuration = 0.2;
+  Toast.$container = undefined;
+  Toast.$template = undefined;
+
+  /**
+   * display a toast
+   */
+  Toast.prototype.display = function() {
+    var $toast;
+
+    if (!initialized) {
+      console.error('Cannot display Toast: '
+          + 'Toast.init() has not been called yet.');
+      return;
+    }
+
+    if (this.$toast) {
+      console.error('toast is already visible');
+      return;
+    }
+
+    $toast = this.$toast = Toast.$template.clone().removeClass('hidden');
+
+    // decide between text and jquery object handle
+    if (Type.isString(this.message)) {
+      this.$toast.text(this.message);
+    } else {
+      this.$toast.append(this.message);
+    }
+
+    // insert the toast and a line break
+    Toast.$container.append(this.$toast);
+    Toast.$container.append('<br>');
+
+    // let the toast fade in
+    window.setTimeout(function() {
+      $toast.addClass('toast');
+    }, 10);
+
+    // Let the toast fade out if it's not infinite
+    if (this.duration > 0) {
+      window.setTimeout(this.close.bind(this),
+          1000 * (this.duration + Toast.fadeinDuration));
+    }
+  };
+
+  /**
+   * create a function which closes the toast
+   * 
+   * @param id
+   *          the toast id
+   * @return a close function
+   */
+  Toast.prototype.close = function() {
+    var i, $alltoasts, $toast;
+
+    if (initialized && this.$toast) {
+      $toast = this.$toast;
+
+      // let the toast fade out
+      $toast.removeClass('toast');
+
+      // remove the toast after fadeout
+      window.setTimeout(function() {
+        $toast.next().remove();
+        $toast.remove();
+      }, 1000 * Toast.fadeoutDuration);
+
+      // forbid the close function from fading out a second time
+      this.$toast = undefined;
+    } else if (pending && pending.indexOf(this) !== -1) {
+      pending.splice(pending.indexOf(this), 1);
+    }
+  };
+
+  /**
+   * initialize the toast. This function has to be called explicitly, or the
+   * unit tests show toasts for every error.
+   */
+  Toast.init = function() {
+    var transition, duration, id;
+    // create toast container
+    Toast.$container = $('<div id="toasts">');
+    Toast.$template = $('<div class="hidden">ERROR</div>');
+    Toast.$container.append(Toast.$template);
+    $('body').append(Toast.$container);
 
     // abort if the style is not set
     if ($('#toasts').css('position') !== 'fixed') {
-      console.error('Toast: stylesheet not found');
-      toastfn = function(str, seconds) {
-        console.error('Toast: ' + str);
-        return getid();
-      };
+      console.error('Toast: stylesheet not found. Initialization failed.');
     } else {
-      // read fade durations from css
-      $hidden = $('#toasts .hidden');
+      Toast.fadeoutDuration = getTransitionDuration();
+      Toast.$template.addClass('toast');
+      Toast.fadeinDuration = getTransitionDuration();
 
-      fadeout = getTransitionDuration();
-
-      $hidden.addClass('toast');
-
-      fadein = getTransitionDuration();
-
-      if (fadein != Number(fadein) || !isFinite(fadein) || isNaN(fadein)) {
-        console.error('fadein: not a valid number: ' + fadein);
-        $hidden.show();
+      if (Toast.fadeinDuration != Number(Toast.fadeinDuration)
+          || !isFinite(Toast.fadeinDuration) || isNaN(Toast.fadeinDuration)) {
+        console.error('Toast.fadeinDuration: not a valid number: '
+            + Toast.fadeinDuration);
+        Toast.$template.removeClass('hidden');
       }
 
-      if (fadeout != Number(fadeout) || !isFinite(fadeout) || isNaN(fadeout)) {
-        console.error('fadeout: not a valid number: ' + fadeout);
-        $hidden.show();
+      if (Toast.fadeoutDuration != Number(Toast.fadeoutDuration)
+          || !isFinite(Toast.fadeoutDuration) || isNaN(Toast.fadeoutDuration)) {
+        console.error('Toast.fadeoutDuration: not a valid number: '
+            + Toast.fadeoutDuration);
+        Toast.$template.removeClass('hidden');
       }
-
-      // actual Toast constructor
-      toastfn = function(str, seconds) {
-        var $toasts, $div, $br;
-
-        // default to two seconds duration
-        seconds = seconds || Toast.SHORT;
-
-        $toasts = $('#toasts');
-        $div = $('<div></div>');
-
-        // decide between text and jquery object handle
-        if (typeof str === 'string') {
-          $div.text(str);
-        } else {
-          $div.append(str);
-        }
-
-        // insert the toast and a line break
-        $toasts.append($div);
-        $div.after($('<br>'));
-
-        // fadein timeout
-        window.setTimeout(function() {
-          $div.addClass('toast');
-        }, 10);
-
-        // remove the toast if it's not infinite
-        if (seconds > 0) {
-          // fadeout timeout
-          window.setTimeout(function() {
-            fadeoutfn($div);
-          }, 1000 * (seconds + fadein));
-        }
-
-        id = getid();
-        $div.data().id = id;
-        return id;
-      };
-
-      fadeoutfn = function($div) {
-        // remove timeout
-        $div.removeClass('toast');
-        window.setTimeout(function() {
-          $div.next().remove();
-          $div.remove();
-        }, 1000 * fadeout);
-      };
     }
+
+    initialized = true;
 
     // issue any pending toasts
     pending.forEach(function(toast) {
-      if (toast) {
-        new Toast(toast.str, toast.seconds);
-      }
+      toast.display();
     }, this);
+
+    pending = undefined;
   };
 
   Toast.SHORT = 2;
   Toast.LONG = 5;
   Toast.INFINITE = -1;
 
-  // remember: return by reference. The new function will replace the old one
+  // remember: return by reference. The new function will replace the old
+  // one
   return Toast;
 });
