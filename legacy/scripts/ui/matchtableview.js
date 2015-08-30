b71aef4c57fd686c4f2aad2448caf59b06f0ff8b
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-08-30 16:52:26 +0200

    add .running class for running rounds/groups

--- legacy/scripts/ui/matchtableview.js
+++ legacy/scripts/ui/matchtableview.js
@@ -6,22 +6,28 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './listview', 'core/listener'], function(extend,
-    ListView, Listener) {
+define(['lib/extend', './listview', 'core/listener', 'core/matchresult'], //
+function(extend, ListView, Listener, MatchResult) {
   /**
    * Constructor
    */
   function MatchTableView() {
-    var $view = arguments[1];
-    arguments[1] = $view.children('table');
+    var $container = arguments[1];
+    arguments[1] = $container.children('table');
     MatchTableView.superconstructor.apply(this, arguments);
 
-    this.$round = $view.find('.round');
+    this.$container = $container;
 
+    this.$round = this.$container.find('.round');
+
+    this.updateRunningState();
     this.updateGroupNumber();
   }
   extend(MatchTableView, ListView);
 
+  /**
+   * print the group ID as soon as it's available
+   */
   MatchTableView.prototype.updateGroupNumber = function() {
     var view = this;
 
@@ -35,6 +41,36 @@ define(['lib/extend', './listview', 'core/listener'], function(extend,
       this.$round.text(Number(this.model.get(0).getGroup()) + 1);
     }
   };
+
+  MatchTableView.prototype.updateRunningState = function() {
+    var i, isRunning;
+
+    isRunning = false;
+    for (i = 0; i < this.model.length; i += 1) {
+      if (!this.model.get(i).isResult()) {
+        isRunning = true;
+        break;
+      }
+    }
+
+    if (isRunning) {
+      this.$container.addClass('running');
+    } else {
+      this.$container.removeClass('running');
+    }
+
+  };
+
+  /**
+   * callback function, which gets called by insert and remove
+   *
+   * @param emitter
+   * @param event
+   * @param data
+   */
+  MatchTableView.prototype.onresize = function(emitter, event, data) {
+    this.updateRunningState();
+  }
 
   return MatchTableView;
 });
