23f3d50646742f3edb651520886e5baea98a25fa
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-09-10 20:05:28 +0200

    Auto-positioning KO matches in the tree

--- legacy/scripts/ui/komatchresultview.js
+++ legacy/scripts/ui/komatchresultview.js
@@ -6,8 +6,16 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './matchresultview', './koline'], function(extend,
-    MatchResultView, KOLine) {
+define(['lib/extend', './matchresultview', './koline', //
+'core/kotournamentmodel'], function(extend, MatchResultView, KOLine,
+    KOTournamentModel) {
+  var leftPadding, topPadding, width, height;
+
+  width = 17;
+  height = 4;
+  topPadding = -1.5;
+  leftPadding = 1;
+
   /**
    * Constructor
    *
@@ -21,16 +29,69 @@ define(['lib/extend', './matchresultview', './koline'], function(extend,
    *          a TournamentModel instance
    */
   function KOMatchResultView(model, $view, teamlist, tournament, showNames) {
-    KOMatchResultView.superconstructor.call(this, model, $view, teamlist,
-        tournament);
+    if ($view) {
+      KOMatchResultView.superconstructor.call(this, model, $view, teamlist,
+          tournament);
+    } else {
+      this.model = model;
+    }
 
-    // TODO set the match position from its ID
-    this.position = undefined;
+    this.id = this.model.getID();
+    this.group = this.model.getGroup();
+    this.round = KOTournamentModel.roundOfMatchID(this.id);
+    this.isThirdPlace = (this.group & 0x1) === 1;
+    this.firstid = KOTournamentModel.firstMatchIDOfRound(this.round);
+    this.numTeams = tournament.getTeams().length;
+    this.firstRound = Math.min(//
+    KOTournamentModel.initialRoundForTeams(this.numTeams), //
+    KOTournamentModel.roundsInGroup(this.group & ~0x1) - 1 //
+    );
 
-    // TODO print the KOLine
-    this.line = undefined;
+    console.log(this.firstRound)
+
+    this.x = this.calcXPosition();
+    this.y = this.calcYPosition();
+
+    if (this.$view) {
+      this.$view.css('left', this.x + 'em');
+      this.$view.css('top', this.y + 'em');
+
+      // TODO print the KOLine
+      this.line = undefined;
+    }
   }
   extend(KOMatchResultView, MatchResultView);
+
+  KOMatchResultView.prototype.calcXPosition = function() {
+    return leftPadding + (this.firstRound - this.round) * width;
+  };
+
+  KOMatchResultView.prototype.calcYPosition = function() {
+    var y, yFactor;
+
+    yFactor = Math.pow(2, this.firstRound - this.round - 1);
+
+    // padding
+    y = topPadding;
+
+    // position of first match in this round
+    y += height * yFactor;
+
+    // offset from first match in this round
+    y += height * 2 * yFactor * (this.id - this.firstid);
+
+    // third place offset
+    if (this.isThirdPlace) {
+      y += height * 1.5;
+    }
+
+    return y;
+  };
+
+  KOMatchResultView.width = width;
+  KOMatchResultView.height = height;
+  KOMatchResultView.topPadding = topPadding;
+  KOMatchResultView.leftPadding = leftPadding;
 
   return KOMatchResultView;
 });
--- legacy/scripts/ui/kotreeview.js
+++ legacy/scripts/ui/kotreeview.js
@@ -6,8 +6,9 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', './templateview', './listview', './komatchresultview'], //
-function(extend, TemplateView, ListView, KOMatchResultView) {
+define(['lib/extend', './templateview', './listview', './komatchresultview',
+    'core/matchmodel', 'core/kotournamentmodel'], function(extend,
+    TemplateView, ListView, KOMatchResultView, MatchModel, KOTournamentModel) {
   /**
    * Constructor
    *
@@ -27,14 +28,47 @@ function(extend, TemplateView, ListView, KOMatchResultView) {
     KOTreeView.superconstructor.call(this, model, $view, $view
         .find('.komatchresult.template'));
 
-    console.log(this.$template);
-
+    this.tournament = tournament;
     this.$forest = this.$view.find('.forest');
 
     this.matches = new ListView(this.model, this.$forest, this.$template,
         KOMatchResultView, teamlist, tournament, undefined);
+
+    this.setSize();
   }
   extend(KOTreeView, TemplateView);
+
+  KOTreeView.prototype.setSize = function() {
+    var group, thirdPlaceMatch, lowestMatch, lowestID, firstRound, x, y;
+
+    group = this.model.get(0).getGroup() & ~0x1;
+
+    firstRound = Math.min(//
+    KOTournamentModel.initialRoundForTeams(this.tournament.getTeams().length), //
+    KOTournamentModel.roundsInGroup(group & ~0x1) - 1 //
+    );
+
+    lowestID = KOTournamentModel.firstMatchIDOfRound(firstRound + 1) - 1;
+
+    // TODO think of a prettier way
+    lowestMatch = new MatchModel([undefined, undefined], lowestID, group);
+    thirdPlaceMatch = new MatchModel([undefined, undefined], 1, group + 1);
+
+    // TODO think of a prettier way
+    lowestMatch = new KOMatchResultView(lowestMatch, undefined, undefined,
+        this.tournament, undefined);
+    thirdPlaceMatch = new KOMatchResultView(thirdPlaceMatch, undefined,
+        undefined, this.tournament, undefined);
+
+    x = thirdPlaceMatch.x;
+    y = Math.max(lowestMatch.y, thirdPlaceMatch.y);
+
+    x += KOMatchResultView.width;
+    y += KOMatchResultView.height;
+
+    this.$forest.css('width', x + 'em');
+    this.$forest.css('height', y + 'em');
+  };
 
   return KOTreeView;
 });
