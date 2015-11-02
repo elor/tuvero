ea954380c751a45ecada04a3a60b12cef92b2317
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-11-02 20:17:55 +0100

    TournamentView constructor cleanup

--- legacy/scripts/ui/tournamentview.js
+++ legacy/scripts/ui/tournamentview.js
@@ -19,7 +19,7 @@ define(['lib/extend', 'core/view', './stateclassview', 'core/listener',
    * @param $view
    */
   function TournamentView(tournament, $view, tournaments) {
-    var $advancedOptions, availableComponents;
+    var $advancedOptions;
     TournamentView.superconstructor.call(this, undefined, $view);
 
     this.$view.attr('rowspan', tournament.getTeams().length);
@@ -34,6 +34,11 @@ define(['lib/extend', 'core/view', './stateclassview', 'core/listener',
     this.$round = this.$view.find('.round');
     this.$nextround = this.$view.find('.nextround');
 
+    this.$initial = this.$view.find('.initial');
+    this.$running = this.$view.find('.running');
+    this.$idle = this.$view.find('.idle');
+    this.$finished = this.$view.find('.finished');
+
     this.$advancedOptions = this.$view.find('.tournamentoptions.boxview');
     if (this.$advancedOptions.length > 0) {
       this.advancedOptions = $advancedOptions = [];
@@ -42,32 +47,7 @@ define(['lib/extend', 'core/view', './stateclassview', 'core/listener',
       });
     }
 
-    availableComponents = Presets.ranking.components.slice(0);
-    tournament.getRanking().get().components.forEach(function(component) {
-      if (availableComponents.indexOf(component) === -1) {
-        availableComponents.push(component);
-      }
-    });
-
-    availableComponents.sort(function(a, b) {
-      a = Strings['ranking_' + a] || a;
-      b = Strings['ranking_' + b] || b;
-      return a.localeCompare(b);
-    });
-
-    this.$rankingOrderView = this.$view.find('.rankingorderview');
-    if (this.$rankingOrderView.length > 0) {
-      this.rankingOrderView = new RankingOrderView(this.model.rankingOrder,
-          this.$rankingOrderView.eq(0), new ListModel(availableComponents));
-      this.rankingOrderView.availableListView.model.erase('headtohead');
-      this.rankingOrderView.availableListView.model.erase('id');
-
-    }
-
-    this.$initial = this.$view.find('.initial');
-    this.$running = this.$view.find('.running');
-    this.$idle = this.$view.find('.idle');
-    this.$finished = this.$view.find('.finished');
+    this.initRankingOrderView();
 
     Listener.bind(this.model.tournament.getName(), 'update', this.updateNames
         .bind(this));
@@ -79,10 +59,45 @@ define(['lib/extend', 'core/view', './stateclassview', 'core/listener',
   }
   extend(TournamentView, View);
 
+  /**
+   * initiate a RankingOrderView, if necessary
+   */
+  TournamentView.prototype.initRankingOrderView = function() {
+    var availableComponents;
+
+    this.$rankingOrderView = this.$view.find('.rankingorderview');
+    if (this.$rankingOrderView.length > 0) {
+      availableComponents = Presets.ranking.components.slice(0);
+      this.model.tournament.getRanking().get().components.forEach(function(
+          component) {
+        if (availableComponents.indexOf(component) === -1) {
+          availableComponents.push(component);
+        }
+      });
+
+      availableComponents.sort(function(a, b) {
+        a = Strings['ranking_' + a] || a;
+        b = Strings['ranking_' + b] || b;
+        return a.localeCompare(b);
+      });
+
+      this.rankingOrderView = new RankingOrderView(this.model.rankingOrder,
+          this.$rankingOrderView.eq(0), new ListModel(availableComponents));
+      this.rankingOrderView.availableListView.model.erase('headtohead');
+      this.rankingOrderView.availableListView.model.erase('id');
+    }
+  };
+
+  /**
+   * update the displayed tournament name
+   */
   TournamentView.prototype.updateNames = function() {
     this.$name.text(this.model.tournament.getName().get());
   };
 
+  /**
+   * update the displayed tournament round
+   */
   TournamentView.prototype.updateRound = function() {
     if (this.model.tournament.getRound) {
       this.$round.text(this.model.tournament.getRound() + 1);
@@ -99,6 +114,12 @@ define(['lib/extend', 'core/view', './stateclassview', 'core/listener',
     if (this.subcontroller && this.subcontroller.destroy) {
       this.subcontroller.destroy();
     }
+
+    if (this.rankingOrderView) {
+      this.rankingOrderView.destroy();
+    }
+
+    this.stateClassView.destroy();
 
     TournamentView.superclass.destroy.call(this);
   };
