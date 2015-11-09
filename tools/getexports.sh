c10a029ea027f016765af0e0f91bcfc4cc90423b
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-11-09 20:11:34 +0100

    getexports.sh: also extracting library names

--- tools/getexports.sh
+++ tools/getexports.sh
@@ -8,20 +8,28 @@ listscripts(){
     find core/scripts/ legacy/scripts/ lib/ -type f -name '*.js' | sort
 }
 
+islib(){
+    local file=$1
+    grep '^lib/' <<< $file &>/dev/null
+}
+
 istest(){
     local file=$1
-    grep -l '/test/' <<< $file &>/dev/null
+    grep '/test/' <<< $file &>/dev/null
 }
 
 ismodule(){
     local file=$1
-    grep -l '^define(\' $file &>/dev/null
+    grep -l '^define(\' $file &>/dev/null || islib $file
 }
 
 getexport(){
     local file=$1
 
-    grep -B1 '^});' $file | sed -n 's#^\s*return\s*\([a-zA-Z]*\);$#\1#p' | grep -v '^undefined$'|| :
+    exported=$(grep -B1 '^});' $file | sed -n 's#^\s*return\s*\([a-zA-Z]*\);$#\1#p' | grep -v '^undefined$'|| :)
+    [ -z "$exported" ] && exported=$(grep -o '^var [a-zA-Z]*' $file | sed -e 's/var //')
+    [ -z "$exported" ] && islib $file && exported=$(basename $file | sed 's/\.js$//')
+    echo "$exported"
 }
 
 getmodule(){
