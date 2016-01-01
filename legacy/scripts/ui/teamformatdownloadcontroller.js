83e2667cf4557020ad89ffe995dbedfd0f106be3
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2016-01-01 16:56:57 +0100

    file registration format download: respecting teamsize

--- legacy/scripts/ui/teamformatdownloadcontroller.js
+++ legacy/scripts/ui/teamformatdownloadcontroller.js
@@ -7,20 +7,28 @@
  * @see LICENSE
  */
 define(['lib/extend', 'core/controller', 'lib/FileSaver', 'lib/Blob',
-    './toast', './strings', 'presets'], function(extend, Controller, saveAs,
-    Blob, Toast, Strings, Presets) {
-  var examplefile;
+    './toast', './strings', 'presets', './state_new'], function(extend,
+    Controller, saveAs, Blob, Toast, Strings, Presets, State) {
+  var examplefiles;
 
-  examplefile = '# Ein Team pro Zeile, Spielernamen mit Komma getrennt\n'
-      + '# Alle Teams müssen gleiche Größe haben\n'
-      + 'Erik E. Lorenz, Fabian \"Fabe\" Böttcher\n'
-      + 'Spieler 3, \"Spieler 4\"\n';
+  examplefiles = [];
+  examplefiles[1] = 'Teilnehmer 1\n' + 'Teilnehmer 2\n' + 'Teilnehmer 3\n'
+      + 'Teilnehmer 4\n' + '\n# Hinweis: Tete-a-tete -> ein Name pro Zeile\n';
+
+  examplefiles[2] = 'Erik E. Lorenz, Fabian "Fabe" Böttcher\n'
+      + 'Spieler 3, Spieler 4\n' + 'Spieler 5, Spieler 6\n'
+      + 'Spieler 7, Spieler 8\n'
+      + '\n# Hinweis: Doublette -> Zwei Namen pro Zeile, mit Komma getrennt\n';
+
+  examplefiles[3] = 'Erik E. Lorenz, Fabian "Fabe" Böttcher, Spieler 3\n'
+      + 'Spieler 4, Spieler 5, Spieler 6\n'
+      + 'Spieler 7, Spieler 8, Spieler 9\n'
+      + '\n# Hinweis: Triplette -> Drei Namen pro Zeile, mit Komma getrennt\n';
 
   /**
    * Constructor
    */
   function TeamFormatDownloadController(view) {
-    var controller;
     TeamFormatDownloadController.superconstructor.call(this, view);
 
     this.view.$view.click(this.save.bind(this));
@@ -31,7 +39,7 @@ define(['lib/extend', 'core/controller', 'lib/FileSaver', 'lib/Blob',
     var blob;
 
     try {
-      blob = new Blob([examplefile]);
+      blob = new Blob([examplefiles[State.teamsize.get()] || examplefiles[2]]);
       saveAs(blob, Presets.names.teamsfile);
     } catch (e) {
       console.error(e.stack);
