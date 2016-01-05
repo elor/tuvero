bef53f8b44cbd8a7a3d624ef6492fa1358a75077
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2016-01-05 10:08:47 +0100

    plato update 2016-01-05

---
+++ plato/files/legacy_scripts_ui_popoutboxview_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"2016-01-04T20:39:46.000Z","sloc":23,"lloc":11,"functions":2,"deliveredBugs":0.1378646667051283,"maintainability":72.04064221611694,"lintErrors":[],"difficulty":7}]
