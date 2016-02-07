74e680ff21571e73fb320ab12a5d4e890ac0ca09
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2016-02-07 11:55:01 +0100

    'strings' dependency for Tuvero Test added

---
+++ test/scripts/strings.js
@@ -0,0 +1,15 @@
+/**
+ * No Description
+ *
+ * @return LocalString
+ * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
+ * @license MIT License
+ * @see LICENSE
+ */
+
+define(function() {
+  return {
+    teamsicon: 'teams',
+    variant: 'Test'
+  };
+});
