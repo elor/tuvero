5f4d4fb13432048e2eeb90d042023b7ac4dda8b0
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2016-04-07 19:44:38 +0200

    Tournament Tree renamed
    Fixes #251

--- legacy/scripts/ui/timemachinecommitcontroller.js
+++ legacy/scripts/ui/timemachinecommitcontroller.js
@@ -16,10 +16,16 @@ function(extend, Controller, TimeMachine, StateLoader, Strings, Toast,
   function TimeMachineCommitController(view) {
     TimeMachineCommitController.superconstructor.call(this, view);
 
+    this.$rename = this.view.$view.find('input.rename');
+
     this.view.$view.find('button.removecommit').click(this.remove.bind(this));
     this.view.$view.find('button.loaddescendant').click(this.load.bind(this));
     this.view.$view.find('button.cleanuptree').click(this.cleanup.bind(this));
     this.view.$view.find('button.download').click(this.download.bind(this));
+
+    this.view.$view.find('.name').click(this.startRename.bind(this));
+    this.$rename.blur(this.endRename.bind(this));
+    this.$rename.keydown(this.renameKey.bind(this));
   }
   extend(TimeMachineCommitController, Controller);
 
@@ -57,6 +63,45 @@ function(extend, Controller, TimeMachine, StateLoader, Strings, Toast,
         || this.model);
     if (!fileSaver.save()) {
       new Toast(Strings.savefailed);
+    }
+  };
+
+  TimeMachineCommitController.prototype.rename = function(name) {
+    if (name && name.length && name != this.model.getTreeName()) {
+      this.model.setTreeName(name);
+    }
+  }
+
+  TimeMachineCommitController.prototype.startRename = function(evt) {
+    this.$rename.val(this.model.getTreeName());
+
+    this.view.$view.addClass('renaming');
+
+    this.$rename.focus();
+
+    if (evt) {
+      evt.preventDefault();
+      return false;
+    }
+  };
+
+  TimeMachineCommitController.prototype.endRename = function() {
+    this.rename(this.$rename.val());
+
+    this.view.$view.removeClass('renaming');
+  };
+
+  TimeMachineCommitController.prototype.renameKey = function(evt) {
+    if (!evt) {
+      return;
+    }
+
+    switch (evt.which) {
+    case 27: // escape
+      this.$input.val(this.model.getTreeName());
+    case 13: // enter
+      this.endRename();
+      break;
     }
   };
 
