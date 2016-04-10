fa21c018684d0f6d2c0ca0962707b7d89217a356
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2016-04-10 12:01:03 +0200

    TournamentController extends RenameController

--- legacy/scripts/ui/tournamentcontroller.js
+++ legacy/scripts/ui/tournamentcontroller.js
@@ -6,8 +6,8 @@
  * @license MIT License
  * @see LICENSE
  */
-define(['lib/extend', 'jquery', 'core/controller', './toast', './strings'], //
-function(extend, $, Controller, Toast, Strings) {
+define(['lib/extend', 'jquery', 'ui/renamecontroller', './toast', './strings'], //
+function(extend, $, RenameController, Toast, Strings) {
   var pendingNameChange;
 
   pendingNameChange = undefined;
@@ -29,25 +29,6 @@ function(extend, $, Controller, Toast, Strings) {
 
     this.$runbutton = this.view.$view.find('button.runtournament');
     this.$closebutton = this.view.$view.find('button.closetournament');
-    this.$toptitle = this.view.$view.find('>h3:first-child');
-    this.$nameinput = this.$toptitle.find('input');
-
-    this.$nameinput.blur(this.closeNameInput.bind(this));
-    this.$nameinput.keydown(function(e) {
-      switch (e.which) {
-      case 13: // enter
-        $(this).blur();
-        break;
-      case 27: // escape
-        $(this).val(tournament.getName().get());
-        $(this).blur();
-        break;
-      default:
-        return true;
-      }
-      e.preventDefault();
-      return false;
-    });
 
     this.$runbutton.click(function() {
       if (tournament.getState().get() === 'initial') {
@@ -73,64 +54,35 @@ function(extend, $, Controller, Toast, Strings) {
       }
     });
 
-    this.$toptitle.click(this.showNameInput.bind(this));
-
     if (pendingNameChange === this.model.tournament) {
-      this.showNameInput();
-      window.setTimeout(this.$nameinput.focus.bind(this.$nameinput), 1);
-      window.setTimeout(this.$nameinput.select.bind(this.$nameinput), 1);
+      this.view.$view.find('.rename').eq(0).click();
+      window.setTimeout(this.$rename.focus.bind(this.$rename), 1);
+      window.setTimeout(this.$rename.select.bind(this.$rename), 1);
     }
   }
-  extend(TournamentController, Controller);
+  extend(TournamentController, RenameController);
 
-  TournamentController.prototype.showNameInput = function() {
-    this.$nameinput.val(this.model.tournament.getName().get());
-    this.$toptitle.addClass('rename');
-
-    this.$nameinput.focus();
-    this.$nameinput.select();
-
-    if (this.toast) {
-      this.toast.display();
-    } else {
-      this.toast = new Toast(Strings.namechangeprompt, Toast.INFINITE);
-    }
+  TournamentController.prototype.getName = function() {
+    return this.model.tournament.getName().get();
   };
 
-  TournamentController.prototype.closeNameInput = function() {
-    var name;
-
-    if (!this.$toptitle.hasClass('rename')) {
-      return;
+  TournamentController.prototype.setName = function(name) {
+    if (!name) {
+      return false;
     }
 
-    name = this.$nameinput.val();
-    name = name.replace(/^\s+|\s+$/, '');
-    if (name) {
-      this.model.tournament.getName().set(name);
-      // TODO move into separate listener class/object (own file or something)
-      new Toast(Strings.namechanged.replace('%s', this.model.tournament
-          .getName().get()));
-    } else {
-      new Toast(Strings.namechangeaborted);
-    }
-    this.$toptitle.removeClass('rename');
-
-    this.toast.close();
+    this.model.tournament.getName().set(name);
 
     pendingNameChange = undefined;
+
+    return true;
   };
 
-  /**
-   * @param tournament
-   *          a TournamentModel instance
-   */
   TournamentController.initiateNameChange = function(tournament) {
     pendingNameChange = tournament;
   };
 
   TournamentController.prototype.destroy = function() {
-    this.closeNameInput();
   };
 
   return TournamentController;
