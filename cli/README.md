988cc843162579be4579a467f6a055b45148f47d
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-04-21 21:36:51 +0200

    cli project: First successful version of Tuvero-CLI

---
+++ cli/README.md
@@ -0,0 +1,5 @@
+﻿# tuvero-cli
+
+Tuvero, the tournament management software, is a package for the planning
+tournaments, drawing matches and calculating results. Tuvero features multiple
+tournament systems, has presets for different sports and board games.
---
+++ cli/index.js
@@ -0,0 +1,54 @@
+﻿#!/usr/bin/env node
+
+"use strict";
+
+var fs = require('fs');
+
+function loadState(file, callback, errcallback) {
+    var requirejs = require('requirejs')
+
+    callback = callback || console.log.bind(console);
+    errcallback = errcallback || console.error.bind(console);
+
+    try {
+        var fileContents = JSON.parse(fs.readFileSync(file, 'utf-8'));
+    } catch (e) {
+        errcallback('cannot read file ' + file);
+        return;
+    }
+
+    var target = fileContents.target;
+
+    requirejs.config({
+        baseUrl: 'scripts',
+        paths: {
+            'core': '../../core/scripts/'
+        },
+        nodeRequire: require
+    });
+
+    requirejs(['core/config'], function (config) {
+        var myBase = '../../' + target + '/scripts/';
+
+        requirejs.config({
+            paths: {
+                'options': myBase + 'options',
+                'presets': myBase + 'presets',
+                'strings': myBase + 'strings'
+            }
+        });
+
+        var State = requirejs('ui/state');
+
+        fs.readFile(file, { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                errcallback(err)
+            } else {
+                State.restore(JSON.parse(data));
+                callback(State);
+            }
+        });
+    });
+}
+
+exports.load = loadState;
