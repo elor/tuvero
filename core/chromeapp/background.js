5343eaef85bc37197e7998782ca793a6f27ec698
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-05-06 12:48:33 +0200

    Chromeapp removed

--- core/chromeapp/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        'outerBounds': {
-            width: 1366,
-            height: 800
-        }
-    });
-});
