2714e29e15f995d9a85b2fd7daca81a36cf78704
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-05-24 22:44:31 +0200

    qunit-test subdirectory removed. Tests have been moved to cli/test.js for now

--- qunit-test/test/app.js
+++
@@ -1,7 +0,0 @@
-QUnit.test("app test", function (assert) {
-    assert.ok(1 === 1, 'stupid test');
-
-    var app = require('../app.js');
-    assert.equal(app.do(1, 1), 2, '1 + 1');
-    assert.equal(app.do(5, 8), 13, '5 + 8');
-});
--- qunit-test/test/app2.js
+++
@@ -1,5 +0,0 @@
-QUnit.test("app test", function (assert) {
-    var app = require('../app.js');
-
-    assert.ok(app.secondFunction() === 'covered', 'covered is covered.');
-});
