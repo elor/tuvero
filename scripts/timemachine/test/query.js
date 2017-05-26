9bd1f27ce2604459c4c7f76922f327a2ca3b0d67
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-05-26 13:02:56 +0200

    timemachine/test/query.js: using Query.source to override window.localStorage in NodeJS

--- scripts/timemachine/test/query.js
+++ scripts/timemachine/test/query.js
@@ -6,13 +6,30 @@
  * @license MIT License
  * @see LICENSE
  */
-define(function() {
-  return function(QUnit, getModule) {
-    var Query, KeyModel, Type;
+define(function () {
+  return function (QUnit, getModule) {
+    var Query, KeyModel, Type, localStorage;
+
+    try {
+      localStorage = window.localStorage;
+    } catch (e) {
+      window = {
+        localStorage: {
+          setItem: function (key, value) {
+            this[key] = value;
+          },
+          removeItem: function (key) {
+            delete this[key];
+          }
+        }
+      };
+    }
 
     Query = getModule('timemachine/query');
     KeyModel = getModule('timemachine/keymodel');
     Type = getModule('core/type');
+
+    Query.source = localStorage;
 
     QUnit.test('Query', function (assert) {
       var query, key, key2, key3, key4, key5, ref, date, res;
@@ -22,7 +39,7 @@ define(function() {
       assert.ok(query, 'ALLKEYS construction successful');
       assert.ok(Type.isArray(query.filter()), 'filter() result is an array');
 
-      query.filter().forEach(function(key) {
+      query.filter().forEach(function (key) {
         var match = /test_/.test(key);
         assert.equal(match, true, 'saved key matches target: ' + key);
         if (match) {
@@ -33,7 +50,7 @@ define(function() {
       });
 
       assert.deepEqual(query.filter(), [],
-          'ALL-query on cleared localStorage returns no results');
+        'ALL-query on cleared localStorage returns no results');
 
       ref = 'test_2016-02-12T12:10:11.591Z_2016-02-12T12:10:11.591Z';
       key = KeyModel.fromString(ref);
@@ -52,13 +69,14 @@ define(function() {
       }
 
       ref = [key.toString(), key2.toString(), key3.toString(), key4.toString()]
-          .sort();
+        .sort();
 
       assert.deepEqual(query.filter(), ref, 'ALLKEYS returns all 4 test keys');
 
+      ref = [key.toString(), key3.toString()].sort();
+
       query = new Query(Query.ROOTKEYS);
       assert.ok(query, 'ROOTKEYS construction successful');
-      ref = [key.toString(), key3.toString()].sort();
       assert.deepEqual(query.filter(), ref, 'ROOTKEYS finds both init keys');
 
       query = new Query(Query.LASTKEYS);
@@ -66,8 +84,7 @@ define(function() {
       ref = [key2.toString(), key4.toString()].sort();
       assert.deepEqual(query.filter(), ref, 'LASTKEYS finds both last saves');
 
-      query = new Query(//
-      Query.LATESTSAVE);
+      query = new Query(Query.LATESTSAVE);
       assert.ok(query, 'LATESTKEYS construction successful');
       ref = [key4.toString()].sort();
       assert.deepEqual(query.filter(), ref, 'LATESTSAVE finds the latest save');
@@ -99,8 +116,8 @@ define(function() {
         window.localStorage.removeItem(key4);
       }
 
-      assert.deepEqual((new Query()).filter(), [],
-          'ALL-query on post-test cleared localStorage returns no results');
+      assert.deepEqual((new Query(undefined)).filter(), [],
+        'ALL-query on post-test cleared localStorage returns no results');
     });
   };
 });
