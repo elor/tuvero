699d337953d06e3a98b3b740a992cc345da7f74d
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-05-26 15:51:43 +0200

    Gulp: `build` tasks extracted to their own file (gulp-tools/buils.js)

---
+++ gulp-tools/build.js
@@ -0,0 +1,60 @@
+﻿'use strict';
+
+var filecount = require('./filecount');
+var run = require('gulp-run');
+var gulp = require('gulp');
+var manifest = require('gulp-manifest');
+var buildstyle = require('./buildstyle');
+var buildconfig = require('./create-build-config');
+
+var targets = ['basic', 'boule', 'tac'];
+
+var extension = '.cmd';
+
+module.exports = function () {
+  gulp.task('build-static', ['build-static-images', 'build-static-html', 'build-static-manifest']);
+
+  gulp.task('build-static-html', function () {
+    return gulp.src('index.html')
+      .pipe(filecount())
+      .pipe(gulp.dest('build/'));
+  });
+
+  gulp.task('build-static-images', function () {
+    return gulp.src('images/*.png')
+      .pipe(filecount())
+      .pipe(gulp.dest('build/images/'));
+  });
+
+  gulp.task('build-static-manifest', function () {
+    return gulp.src(['index.html', 'images/*.png'], { base: './' })
+      .pipe(filecount())
+      .pipe(manifest({ filename: 'manifest.appcache', timestamp: false, hash: true }))
+      .pipe(filecount())
+      .pipe(gulp.dest('build/'));
+  });
+
+  targets.forEach(function (target) {
+    gulp.task(`build-${target}`, [
+      `build-${target}-images`,
+      `build-${target}-index`,
+      `build-${target}-scripts`,
+      `build-${target}-style`
+    ]);
+
+    gulp.task(`build-${target}-style-internal`, buildstyle(target));
+
+    gulp.task(`build-${target}-style`, ['update-mainstyle'], function () {
+      return run(`gulp build-${target}-style-internal`).exec();
+    });
+  });
+
+  gulp.task('build-config', function () {
+    return gulp.src(['*/scripts/main.js'])
+      .pipe(buildconfig())
+      .pipe(gulp.dest('build/config/'));
+  });
+
+  return ['build-static', 'build-boule', 'build-basic', 'build-tac'];
+};
+module.exports.targets = targets;
--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,9 @@ var run = require('gulp-run');
 var gulp = require('gulp');
 var mainstyle = require('./gulp-tools/mainstyle');
 var checkdependencies = require('./gulp-tools/check-dependencies');
-var manifest = require('gulp-manifest');
 var template = require('./gulp-tools/template');
 var libs = require('./gulp-tools/libs');
-var buildstyle = require('./gulp-tools/buildstyle');
-var multiprocess = require('gulp-multi-process');
+var build = require('./gulp-tools/build');
 
 var sources = {
   styles: ['lib/*.css', 'style/**/*.css', '!style/mainstyle.css'],
@@ -30,54 +28,14 @@ var sources = {
 gulp.task('default', ['lib', 'update', 'build', 'test']);
 gulp.task('update', ['update-mainstyle', 'update-common-js', 'update-test-js', 'template']);
 gulp.task('template', ['template-basic', 'template-boule', 'template-tac']);
-gulp.task('build', ['build-static', 'build-boule', 'build-basic', 'build-tac']);
-gulp.task('test', ['lib'], function () {
+gulp.task('test', ['lib', 'test-dependencies'], function () {
   return run('node cli/test.js').exec();
 });
 
 gulp.task('lib', libs());
+gulp.task('build', build());
 
-gulp.task('build-static', ['build-static-images', 'build-static-html', 'build-static-manifest']);
-
-gulp.task('build-static-html', function () {
-  return gulp.src('index.html')
-    .pipe(filecount())
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('build-static-images', function () {
-  return gulp.src('images/*.png')
-    .pipe(filecount())
-    .pipe(gulp.dest('build/images/'));
-});
-
-gulp.task('build-static-manifest', function () {
-  return gulp.src(['index.html', 'images/*.png'], { base: './' })
-    .pipe(filecount())
-    .pipe(manifest({ filename: 'manifest.appcache', timestamp: false, hash: true }))
-    .pipe(filecount())
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('build-basic', ['template', 'build-basic-style']);
-gulp.task('build-boule', ['template', 'build-boule-style']);
-gulp.task('build-tac', ['template', 'build-tac-style']);
-
-gulp.task('build-basic-style-internal', buildstyle('basic'));
-gulp.task('build-boule-style-internal', buildstyle('boule'));
-gulp.task('build-tac-style-internal', buildstyle('tac'));
-
-gulp.task('build-basic-style', ['update-mainstyle'], function (cb) {
-  return multiprocess(['build-basic-style-internal'], cb);
-});
-gulp.task('build-boule-style', ['update-mainstyle'], function (cb) {
-  return multiprocess(['build-boule-style-internal'], cb);
-});
-gulp.task('build-tac-style', ['update-mainstyle'], function (cb) {
-  return multiprocess(['build-tac-style-internal'], cb);
-});
-
-gulp.task('check-dependencies', function () {
+gulp.task('test-dependencies', function () {
   return gulp.src(sources.dependant_scripts, { base: 'scripts' })
     .pipe(filecount())
     .pipe(checkdependencies());
@@ -108,14 +66,14 @@ gulp.task('update-test-js', [], function () {
     .pipe(gulp.dest('test/scripts'));
 });
 
-gulp.task('template-basic', template('basic', sources.template_path));
-gulp.task('template-boule', template('boule', sources.template_path));
-gulp.task('template-tac', template('tac', sources.template_path));
+build.targets.forEach(function (target) {
+  gulp.task(`template-${target}`, template(target, sources.template_path));
+})
 
 gulp.task('watch', function () {
   gulp.watch(sources.scripts, ['update-common-js']);
   gulp.watch(sources.scripts_and_tests, ['test']);
-  gulp.watch(sources.dependant_scripts, ['check-dependencies']);
+  gulp.watch(sources.dependant_scripts, ['test-dependencies']);
   gulp.watch(sources.styles, ['update-mainstyle']);
   gulp.watch(sources.templates, ['template']);
   gulp.watch(sources.tests, ['update-test-js']);
