da8675eaff1958c9f87ebca6c391fb92fc6a13f8
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-07-10 20:21:47 +0200

    This should be it

--- gulp-tools/sources.js
+++ gulp-tools/sources.js
@@ -34,21 +34,20 @@ var sources = {
   templates: 'templates/*.html',
   template_index: 'templates/index.html',
   release_source_cleanup: [
-    '{basic,boule,tac,test}/**/*',
-    'bower_components/**/*',
-    '{bower.json,ChangeLog,Dockerfile}',
-    'index.html',
-    '{core,tmp,tools}/**/*',
-    '{script,images,style,templates}/**/*',
-    '{cli,mobile,doc}/**/*',
+    '*',
     '.{jshintrc,dockerignore,travis.yml}',
+    '!build',
+    '!gulpfile.js',
+    '!gulp-tools',
+    '!node_modules',
+    '!LICENSE',
+    '!README*'
   ],
   release_final_cleanup: [
-    'package.json',
+    'build',
     'gulpfile.js',
-    'node_modules/**/*',
-    'gulp-tools/**/*',
-    '{tuvero.njsproj,tuvero.sln}',
+    'gulp-tools',
+    'node_modules'
   ]
 };
 
