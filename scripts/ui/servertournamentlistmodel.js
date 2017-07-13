5bfe16831097192eb5df43b5b7e4707ac1c855f1
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-07-13 20:09:50 +0200

    Only showing tournaments if logged in

--- scripts/ui/servertournamentlistmodel.js
+++ scripts/ui/servertournamentlistmodel.js
@@ -7,54 +7,56 @@
  * @see LICENSE
  */
 define(['lib/extend', 'list/listmodel', 'ui/servertournamentmodel', //
-'presets'], function(extend, ListModel, ServerTournamentModel, Presets) {
-  /**
-   * Constructor
-   */
-  function ServerTournamentListModel(server) {
-    ServerTournamentListModel.superconstructor.call(this);
+  'presets'], function (extend, ListModel, ServerTournamentModel, Presets) {
+    /**
+     * Constructor
+     */
+    function ServerTournamentListModel(server) {
+      ServerTournamentListModel.superconstructor.call(this);
 
-    this.server = server;
+      this.server = server;
 
-    this.server.registerListener(this);
-  }
-  extend(ServerTournamentListModel, ListModel);
+      this.server.registerListener(this);
+    }
+    extend(ServerTournamentListModel, ListModel);
 
-  ServerTournamentListModel.prototype.parseResult = function(data) {
-    this.clear();
+    ServerTournamentListModel.prototype.parseResult = function (data) {
+      this.clear();
 
-    Object.keys(data.tournaments).forEach(function(tournamentID) {
-      var tournament, tournamentData;
-      tournamentData = data.tournaments[tournamentID];
-      if (tournamentData.target === Presets.target) {
-        tournament = new ServerTournamentModel(this.server, tournamentData);
-        this.push(tournament);
+      if (data.logged_in) {
+        Object.keys(data.tournaments).forEach(function (tournamentID) {
+          var tournament, tournamentData;
+          tournamentData = data.tournaments[tournamentID];
+          if (tournamentData.target === Presets.target) {
+            tournament = new ServerTournamentModel(this.server, tournamentData);
+            this.push(tournament);
+          }
+        }, this);
       }
-    }, this);
-  };
+    };
 
-  ServerTournamentListModel.prototype.update = function() {
-    var message = this.server.message('t', {publiconly: false, showarchive: true});
+    ServerTournamentListModel.prototype.update = function () {
+      var message = this.server.message('t', { publiconly: false, showarchive: true });
 
-    message.onreceive = (function(emitter, event, data) {
-      this.parseResult(data);
-    }).bind(this);
-    message.onerror = this.clear.bind(this);
+      message.onreceive = (function (emitter, event, data) {
+        this.parseResult(data);
+      }).bind(this);
+      message.onerror = this.clear.bind(this);
 
-    message.send();
-  };
+      message.send();
+    };
 
-  ServerTournamentListModel.prototype.onlogin = function() {
-    this.update();
-  };
+    ServerTournamentListModel.prototype.onlogin = function () {
+      this.update();
+    };
 
-  ServerTournamentListModel.prototype.onlogout = function() {
-    this.clear();
-  };
+    ServerTournamentListModel.prototype.onlogout = function () {
+      this.clear();
+    };
 
-  ServerTournamentListModel.prototype.onerror = function() {
-    this.clear();
-  };
+    ServerTournamentListModel.prototype.onerror = function () {
+      this.clear();
+    };
 
-  return ServerTournamentListModel;
-});
+    return ServerTournamentListModel;
+  });
