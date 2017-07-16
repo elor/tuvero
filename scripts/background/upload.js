28bd8202490c70a35e6554e7b8b20ae7292b95c0
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-07-16 19:11:50 +0200

    upload.js exports a utility function for uploading the current state

--- scripts/background/upload.js
+++ scripts/background/upload.js
@@ -8,34 +8,36 @@
  */
 define(['jquery', 'ui/toast', 'ui/strings', 'ui/server', 'ui/state', 'core/listener'],
   function ($, Toast, Strings, Server, State, Listener) {
-    var Upload;
+    function upload() {
+      var serverlink = State.serverlink.get();
+      var uploadToast;
+
+      if (!Server.logged_in.get()) {
+        new Toast(Strings.not_logged_in);
+      } else if (!serverlink) {
+        new Toast(Strings.state_not_linked);
+      } else {
+        var message = Server.message('/t/' + serverlink + '/state/upload', State.save());
+        uploadToast = new Toast(Strings.state_uploading, Toast.INFINITE);
+        Listener.bind(message, 'error', function (emitter, event, data) {
+          new Toast(Strings.state_upload_failed);
+          console.error(data);
+        });
+        Listener.bind(message, 'receive', function () {
+          new Toast(Strings.state_upload_complete);
+        });
+        Listener.bind(message, 'complete', function (emitter, event, data) {
+          uploadToast.close();
+        });
+        message.send();
+      }
+    }
 
     $(function ($) {
       $('#tabs').on('click', 'button.upload', function () {
-        var serverlink = State.serverlink.get();
-        var uploadToast;
-
-        if (!Server.logged_in.get()) {
-          new Toast(Strings.not_logged_in);
-        } else if (!serverlink) {
-          new Toast(Strings.state_not_linked);
-        } else {
-          var message = Server.message('/t/' + serverlink + '/state/upload', State.save());
-          uploadToast = new Toast(Strings.state_uploading, Toast.INFINITE);
-          Listener.bind(message, 'error', function (emitter, event, data) {
-            new Toast(Strings.state_upload_failed);
-            console.error(data);
-          });
-          Listener.bind(message, 'receive', function () {
-            new Toast(Strings.state_upload_complete);
-          });
-          Listener.bind(message, 'complete', function (emitter, event, data) {
-            uploadToast.close();
-          });
-          message.send();
-        }
+        upload();
       });
     });
 
-    return Upload;
+    return upload;
   });
