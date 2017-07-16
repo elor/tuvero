de33bec9801dbd8607b82aa8b69a95e16b3c0ed5
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-07-16 16:23:51 +0200

    upload.js: Upload button logic finished

---
+++ scripts/background/upload.js
@@ -0,0 +1,35 @@
+/**
+ * Save button logic which initiates a file download of the current state for
+ * later loading
+ *
+ * @author Erik E. Lorenz <erik.e.lorenz@gmail.com>
+ * @license MIT License
+ * @see LICENSE
+ */
+define(['jquery', 'ui/toast', 'ui/strings', 'ui/server', 'ui/state', 'core/listener'],
+  function ($, Toast, Strings, Server, State) {
+    var Upload;
+
+    $(function ($) {
+      $('#tabs').on('click', 'button.upload', function () {
+        var serverlink = !State.serverlink.get();
+
+        if (!Server.logged_in.get()) {
+          new Toast('Nicht angemeldet');
+        } else if (serverlink) {
+          new Toast('Turnier nicht auf dem Server registriert');
+        } else {
+          var message = Server.message('/t/' + serverlink + '/state/upload', State.save());
+          Listener.bind(message, 'error', function () {
+            new Toast('Hochladen fehlgeschlagen');
+          });
+          Listener.bind(message, 'receive', function () {
+            new Toast('Turnierstand hochgeladen');
+          });
+          message.send();
+        }
+      });
+    });
+
+    return Upload;
+  });
