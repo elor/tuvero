2d2e22b8038b893c2bad5ed194cf618b2d27a11c
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2017-10-23 13:15:14 +0200

    Use comprehensible names for ranking points

--- basic/scripts/strings.js
+++ basic/scripts/strings.js
@@ -6,11 +6,20 @@
  * @license MIT License
  * @see LICENSE
  */
-define(function() {
+define(function () {
   return {
     teamsicon: 'teams',
     variant: 'Basic',
     teamtext: 'Team',
-    teamstext: 'Teams'
+    teamstext: 'Teams',
+    ranking_points: 'Spielpunkte',
+    ranking_lostpoints: 'Gegnerpunkte',
+    ranking_saldo: 'Punktedifferenz',
+    ranking_short_points: 'P',
+    ranking_short_lostpoints: 'GP',
+    ranking_short_saldo: 'PD',
+    ranking_medium_points: 'Punkte',
+    ranking_medium_lostpoints: 'Gegnerpunkte',
+    ranking_medium_saldo: 'Differenz'
   };
 });
