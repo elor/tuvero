a90b37086eca1481931acc4de41cdba7facbbff4
Erik E. Lorenz <erik@tuvero.de>
2018-01-27 21:55:14 +0100

    Add placement tournament to generictournamentview

--- scripts/ui/generictournamentview.js
+++ scripts/ui/generictournamentview.js
@@ -7,45 +7,47 @@
  * @see LICENSE
  */
 define(['lib/extend', 'core/view', 'ui/tournamentview', 'ui/roundtournamentview',
-    'ui/swisstournamentview', 'ui/kotournamentview'], function(extend, View, //
-TournamentView, RoundTournamentView, SwissTournamentView, KOTournamentView) {
-  var constructors, defaultConstructor;
+  'ui/swisstournamentview', 'ui/kotournamentview', 'ui/placementtournamentview'],
+  function (extend, View, TournamentView, RoundTournamentView, SwissTournamentView,
+    KOTournamentView, PlacementTournamentView) {
+    var constructors, defaultConstructor;
 
-  constructors = {
-    swiss: SwissTournamentView,
-    round: RoundTournamentView,
-    ko: KOTournamentView
-  };
+    constructors = {
+      swiss: SwissTournamentView,
+      round: RoundTournamentView,
+      ko: KOTournamentView,
+      placement: PlacementTournamentView
+    };
 
-  defaultConstructor = TournamentView;
+    defaultConstructor = TournamentView;
 
-  /**
-   * Constructor
-   */
-  function GenericTournamentView(tournament, $view, tournaments) {
-    var Constructor;
-    GenericTournamentView.superconstructor.call(this, undefined, $view);
-
-    this.tournament = tournament;
-    if (tournament) {
-      Constructor = constructors[tournament.SYSTEM] || defaultConstructor;
-      this.view = new Constructor(tournament, $view, tournaments);
-    } else {
-      this.view = new View(undefined, $view);
-    }
-
-    /*
-     * Note: for some unknown reason, the prototype chain ignores
-     * 'prototype.destroy'
+    /**
+     * Constructor
      */
-    this.destroy = GenericTournamentView.destroy;
-  }
-  extend(GenericTournamentView, View);
+    function GenericTournamentView(tournament, $view, tournaments) {
+      var Constructor;
+      GenericTournamentView.superconstructor.call(this, undefined, $view);
 
-  GenericTournamentView.destroy = function() {
-    this.view.destroy();
-    GenericTournamentView.superclass.destroy.call(this);
-  };
+      this.tournament = tournament;
+      if (tournament) {
+        Constructor = constructors[tournament.SYSTEM] || defaultConstructor;
+        this.view = new Constructor(tournament, $view, tournaments);
+      } else {
+        this.view = new View(undefined, $view);
+      }
 
-  return GenericTournamentView;
-});
+      /*
+       * Note: for some unknown reason, the prototype chain ignores
+       * 'prototype.destroy'
+       */
+      this.destroy = GenericTournamentView.destroy;
+    }
+    extend(GenericTournamentView, View);
+
+    GenericTournamentView.destroy = function () {
+      this.view.destroy();
+      GenericTournamentView.superclass.destroy.call(this);
+    };
+
+    return GenericTournamentView;
+  });
