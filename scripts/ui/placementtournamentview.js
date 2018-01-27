ea9f977860f8fb3a6bccf8e92f742212818d93c4
Erik E. Lorenz <erik@tuvero.de>
2018-01-27 21:55:49 +0100

    Add PlacementTournamentView

---
+++ scripts/ui/placementtournamentview.js
@@ -0,0 +1,16 @@
+/**
+ * PlacementTournamentView
+ *
+ * @return PlacementTournamentView
+ * @author Erik E. Lorenz <erik@tuvero.de>
+ * @license MIT License
+ * @see LICENSE
+ */
+define(['lib/extend', 'ui/tournamentview'], function (extend, TournamentView) {
+  function PlacementTournamentView(model, $view, tournaments) {
+    PlacementTournamentView.superconstructor.call(this, model, $view, tournaments);
+  }
+  extend(PlacementTournamentView, TournamentView);
+
+  return PlacementTournamentView;
+});
