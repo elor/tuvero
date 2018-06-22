81e84d60fbd1873ce2e5efd059eab9caa288e34b
Erik E. Lorenz <erik@tuvero.de>
2018-06-22 16:01:02 +0200

    Extend PoulesTables with modes for barrage and roundrobin for 3 and 4 teams

--- scripts/tournament/ressources/poulestables.js
+++ scripts/tournament/ressources/poulestables.js
@@ -1,72 +1,146 @@
-define({
-  MATCHES: {
-    acbd: {
-      default: [
-        [0, 3],
-        [1, 2],
-        [{
-            from: 0,
-            who: "winner"
-          },
-          {
-            from: 1,
-            who: "winner"
-          }
+define(function () {
+  var roundrobin_byes, winner, loser;
+
+  winner = "winner";
+  loser = "loser";
+  roundrobin_byes = [
+    [0, 2],
+    [1, 2],
+    [0, 1]
+  ];
+
+  return {
+    MATCHES: {
+      acbd: {
+        default: [
+          [0, 3],
+          [1, 2],
+          [{
+              from: 0,
+              who: winner
+            },
+            {
+              from: 1,
+              who: winner
+            }
+          ],
+          [{
+              from: 0,
+              who: loser
+            },
+            {
+              from: 1,
+              who: loser
+            }
+          ]
         ],
-        [{
-            from: 0,
-            who: "loser"
-          },
-          {
+        favorites: [
+          [0],
+          [1, 2],
+          [
+            0,
+            {
+              from: 1,
+              who: winner
+            }
+          ],
+          [{
             from: 1,
-            who: "loser"
-          }
+            who: loser
+          }]
+        ],
+        lastteams: [
+          [0, 1],
+          [2],
+          [{
+              from: 0,
+              who: winner
+            },
+            2
+          ],
+          [{
+            from: 0,
+            who: loser
+          }]
         ]
-      ],
-      favorites: [
-        [0],
-        [1, 2],
-        [
-          0,
-          {
-            from: 1,
-            who: "winner"
-          }
-        ],
-        [{
-          from: 1,
-          who: "loser"
-        }]
-      ],
-      lastteams: [
-        [0, 1],
-        [2],
-        [{
+      },
+      barrage: {
+        default: [
+          [0, 3],
+          [1, 2],
+          [{
             from: 0,
-            who: "winner"
-          },
-          2
+            who: winner
+          }, {
+            from: 1,
+            who: winner
+          }],
+          [{
+            from: 0,
+            who: loser
+          }, {
+            from: 1,
+            who: loser
+          }],
+          [{
+            from: 2,
+            who: loser
+          }, {
+            from: 3,
+            who: winner
+          }]
         ],
-        [{
-          from: 0,
-          who: "loser"
-        }]
+        favorites: roundrobin_byes,
+        lastteams: roundrobin_byes
+      },
+      roundrobin: {
+        default: [
+          [0, 3],
+          [1, 2],
+          [0, 2],
+          [1, 3],
+          [0, 1],
+          [2, 3]
+        ],
+        favorites: roundrobin_byes,
+        lastteams: roundrobin_byes
+      }
+    },
+    RANKING: {
+      acbd: [{
+          from: 2,
+          who: winner
+        },
+        {
+          from: 2,
+          who: loser
+        },
+        {
+          from: 3,
+          who: winner
+        },
+        {
+          from: 3,
+          who: loser
+        }
+      ],
+      barrage: [{
+          from: 2,
+          who: winner
+        },
+        {
+          from: 4,
+          who: winner
+        },
+        {
+          from: 4,
+          who: loser
+        },
+        {
+          from: 3,
+          who: loser
+        }
       ]
     }
-  },
-  RANKING: {
-    acbd: [{
-      from: 2,
-      who: "winner"
-    }, {
-      from: 2,
-      who: "loser"
-    }, {
-      from: 3,
-      who: "winner"
-    }, {
-      from: 3,
-      who: "loser"
-    }]
-  }
+  };
 });
