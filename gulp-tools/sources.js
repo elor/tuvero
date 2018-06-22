4ca6622086c3a61455760bafe7cc186af144d6e5
Erik E. Lorenz <erik@tuvero.de>
2018-06-22 21:59:13 +0200

    Remove .vscode directory during release

--- gulp-tools/sources.js
+++ gulp-tools/sources.js
@@ -1,55 +1,55 @@
 var sources = {
   styles: [
-    'lib/*.css',
-    'style/**/*.css',
-    '!style/mainstyle.css'
+    "lib/*.css",
+    "style/**/*.css",
+    "!style/mainstyle.css"
   ],
   scripts_and_tests: [
-    'scripts/**/*.js',
-    '!scripts/core/{common,config,main}.js',
-    '!**/lib/*.js'
+    "scripts/**/*.js",
+    "!scripts/core/{common,config,main}.js",
+    "!**/lib/*.js"
   ],
   scripts: [
-    'scripts/*/*.js',
-    '!scripts/core/{common,config,main}.js',
-    '!**/{lib,test}/*.js'
+    "scripts/*/*.js",
+    "!scripts/core/{common,config,main}.js",
+    "!**/{lib,test}/*.js"
   ],
   scripts_for_linting: [
-    '*.js',
-    'gulp-tools/*.js',
-    'scripts/**/*.js',
-    '*/scripts/**/*.js',
-    '!*/scripts/**/build.js',
-    '!**/lib/*.js',
-    '!**/{require,qunit}.js'
+    "*.js",
+    "gulp-tools/*.js",
+    "scripts/**/*.js",
+    "*/scripts/**/*.js",
+    "!*/scripts/**/build.js",
+    "!**/lib/*.js",
+    "!**/{require,qunit}.js"
   ],
   dependent_scripts: [
-    'scripts/*/*.js',
-    '{basic,boule,tac,test}/scripts/{presets,options,strings}.js',
-    'scripts/**/test/*.js',
-    '!scripts/core/{common,config,main}.js',
-    '!**/lib/*.js'
+    "scripts/*/*.js",
+    "{basic,boule,tac,test}/scripts/{presets,options,strings}.js",
+    "scripts/**/test/*.js",
+    "!scripts/core/{common,config,main}.js",
+    "!**/lib/*.js"
   ],
-  tests: ['scripts/**/test/*.js'],
-  templates: 'templates/*.html',
-  template_index: 'templates/index.html',
+  tests: ["scripts/**/test/*.js"],
+  templates: "templates/*.html",
+  template_index: "templates/index.html",
   release_source_cleanup: [
-    '*',
-    '.{jshintrc,dockerignore,travis.yml,gitlab-ci.yml,eslintrc.js}',
-    'build/test',
-    '!build',
-    '!gulpfile.js',
-    '!gulp-tools',
-    '!node_modules',
-    '!LICENSE',
-    '!README*'
+    "*",
+    ".{jshintrc,dockerignore,travis.yml,gitlab-ci.yml,eslintrc.js,.vscode}",
+    "build/test",
+    "!build",
+    "!gulpfile.js",
+    "!gulp-tools",
+    "!node_modules",
+    "!LICENSE",
+    "!README*"
   ],
   release_final_cleanup: [
-    'build',
-    'gulpfile.js',
-    'gulp-tools',
-    'node_modules',
-    '.gitignore'
+    "build",
+    "gulpfile.js",
+    "gulp-tools",
+    "node_modules",
+    ".gitignore"
   ]
 };
 
