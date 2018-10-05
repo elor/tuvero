968af17cf4fee22a43ab6a91abf837f242c431ba
Tuvero Autobuild <info@tuvero.de>
2018-10-05 12:21:38 +0000

    Release 1.5.22 built

--- tools/verify-strings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# finds all occurences of "Strings." and "Strings[" and matches them against strings.json to see which strings are unused
-
-git grep 'Strings\.' | grep -Po 'Strings\.[^); .,\]]*' | sed 's/^Strings\.//'
