50e29885020e75003a6818de0f5dd3ee235915fa
Tuvero Autobuild <info@tuvero.de>
2018-10-05 09:15:15 +0000

    Release 1.5.20 built

--- scripts/background/loginstate.js
+++
@@ -1,9 +0,0 @@
-define(["jquery", "ui/server", "core/classview"], function ($, Server, ClassView) {
-    var LoginState = {};
-
-    $(function ($) {
-      LoginState.classView = new ClassView(Server.logged_in, $("body"), "loggedin", "loggedout");
-    });
-
-    return LoginState;
-  });
