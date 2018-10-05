10232948eca84cf5c125d73a722e7515f759d506
Erik E. Lorenz <erik@tuvero.de>
2018-10-05 13:00:48 +0200

    Autoformat GenericTournamentHistoryView

--- scripts/ui/generictournamenthistoryview.js
+++ scripts/ui/generictournamenthistoryview.js
@@ -7,71 +7,71 @@
  * @see LICENSE
  */
 define(["lib/extend", "core/view", "ui/progresstableview", "ui/kohistoryview"], //
-function (extend, View, ProgressTableView, KOHistoryView) {
-  var types;
+  function (extend, View, ProgressTableView, KOHistoryView) {
+    var types;
 
-  types = {
-    swiss: {
-      constructor: ProgressTableView,
-      selector: ".progresstable",
-      showlists: true
-    },
-    round: {
-      constructor: ProgressTableView,
-      selector: ".progresstable",
-      showlists: true
-    },
-    ko: {
-      constructor: KOHistoryView,
-      selector: ".kotree",
-      showlists: false
+    types = {
+      swiss: {
+        constructor: ProgressTableView,
+        selector: ".progresstable",
+        showlists: true
+      },
+      round: {
+        constructor: ProgressTableView,
+        selector: ".progresstable",
+        showlists: true
+      },
+      ko: {
+        constructor: KOHistoryView,
+        selector: ".kotree",
+        showlists: false
+      }
+    };
+
+    /**
+     * Constructor
+     *
+     * @param tournament
+     *          a TournamentModel instance
+     * @param $view
+     *          a DOM element
+     * @param groups
+     *          a ListModel with the group names
+     * @param teamlist
+     *          a ListModel of TeamModels which are referenced by teamIDs
+     * @param teamsize
+     *          a ValueModel of the current default team size
+     * @param fullwidth
+     *          a ValueModel, which evaluates to true if any names should be shown
+     */
+    function GenericTournamentHistoryView(tournament, $view, groups, teamlist,
+      teamsize, fullwidth) {
+      var Constructor, $subview, type;
+      GenericTournamentHistoryView.superconstructor.call(this, undefined, $view);
+
+      this.tournament = tournament;
+      type = types[tournament.SYSTEM];
+      if (tournament && type) {
+        $subview = this.$view.find(type.selector);
+        $subview.removeClass("hidden");
+        // don't display the matchtable on default anymore, since there's a more
+        // sophisticated view in place
+        $view.addClass("hastable");
+        Constructor = type.constructor;
+        this.view = new Constructor(tournament, $view, groups, teamlist, //
+          teamsize, fullwidth);
+        this.showlists = !!type.showlists;
+      } else {
+        this.view = new View(undefined, $view);
+        this.showlists = true;
+      }
     }
-  };
+    extend(GenericTournamentHistoryView, View);
 
-  /**
-   * Constructor
-   *
-   * @param tournament
-   *          a TournamentModel instance
-   * @param $view
-   *          a DOM element
-   * @param groups
-   *          a ListModel with the group names
-   * @param teamlist
-   *          a ListModel of TeamModels which are referenced by teamIDs
-   * @param teamsize
-   *          a ValueModel of the current default team size
-   * @param showNames
-   *          a ValueModel, which evaluates to true if names should be shown
-   */
-  function GenericTournamentHistoryView(tournament, $view, groups, teamlist,
-      teamsize, showNames) {
-    var Constructor, $subview, type;
-    GenericTournamentHistoryView.superconstructor.call(this, undefined, $view);
+    GenericTournamentHistoryView.prototype.destroy = function () {
+      this.view.destroy();
+      GenericTournamentHistoryView.superclass.destroy.call(this);
+    };
 
-    this.tournament = tournament;
-    type = types[tournament.SYSTEM];
-    if (tournament && type) {
-      $subview = this.$view.find(type.selector);
-      $subview.removeClass("hidden");
-      // don't display the matchtable on default anymore, since there's a more
-      // sophisticated view in place
-      $view.addClass("hastable");
-      Constructor = type.constructor;
-      this.view = new Constructor(tournament, $view, groups, teamlist, //
-      teamsize, showNames);
-      this.showlists = !!type.showlists;
-    } else {
-      this.view = new View(undefined, $view);
-      this.showlists = true;
-    }
-  }
-  extend(GenericTournamentHistoryView, View);
-
-  GenericTournamentHistoryView.prototype.destroy = function () {
-    this.view.destroy();
-    GenericTournamentHistoryView.superclass.destroy.call(this);
-  };
-
-  return GenericTournamentHistoryView;
-});
+    return GenericTournamentHistoryView;
+  });
