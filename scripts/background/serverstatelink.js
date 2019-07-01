8080b8b3169b4c4308d51664d7bba20338d20597
Tuvero Autobuild <info@tuvero.de>
2019-07-01 14:33:08 +0000

    Release 1.5.26 built

--- scripts/background/serverstatelink.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'ui/state', 'core/classview'], function ($, State, ClassView) {
-  var ServerStateLink = {}
-
-  $(function ($) {
-    ServerStateLink.classView = new ClassView(State.serverlink, $('body'), 'statelink', 'nostatelink')
-  })
-
-  return ServerStateLink
-})
